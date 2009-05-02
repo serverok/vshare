0df557926c144b35831115474ce7c18773602b10
BuyScripts Admin <info@buyscripts.in>
2009-05-02 15:39:19 +0530

    modified config.php for testing from git. config.php moved outside of werb root, good for production web sites.

--- include/config.php
+++ include/config.php
@@ -1,20 +1,3 @@
 <?php
 
-session_start();
-
-$db_host     = 'localhost';
-$db_name     = 'vshare';
-$db_user     = 'vshare';
-$db_pass     = 'vshare';
-
-$language = "en";
-
-$config = array();
-$config['ffmpeg']          =  '/usr/bin/ffmpeg';
-$config['mplayer']          =  '/usr/bin/mplayer';
-$config['mencoder']          =  '/usr/bin/mencoder';
-$config['flvtool']          =  '/usr/bin/flvtool2';
-$config['basedir']        =  '/home/web/vshare';
-$config['baseurl']        =  'http://linux/web/vshare';
-
-include($config['basedir'] . '/include/vshare.php');
+require '/home/web/config.php';
--- video.php
+++ video.php
@@ -13,6 +13,9 @@
  ******************************************************************************/
 
 require 'include/config.php';
+require 'include/class.cache.php';
+
+Cache::init();
 
 $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
 
@@ -20,6 +23,7 @@ if ($page < 1)
 {
     $page = 1;
 }
+
 
 $view_type = isset($_GET['viewtype']) ? $_GET['viewtype'] : 'basic';
 
@@ -32,7 +36,7 @@ $smarty->assign('viewtype', $view_type);
 
 $category = $_GET['category'];
 
-$allowed_types = array(
+$category_all = array(
     'random',
     'recent',
     'viewed',
@@ -42,13 +46,16 @@ $allowed_types = array(
     'featured'
 );
 
-if (! in_array($category, $allowed_types))
+if (! in_array($category, $category_all))
 {
     $category = 'recent';
 }
 
+$cache_id = 'video_' . $view_type . $category . $page;
+
 if (isset($_GET['chid']))
 {
+    $cache_id .= '_channel_id' . $_GET['chid'];
     $channel_sql = "AND `video_channels` LIKE '%|" . (int) $_GET['chid'] . "|%'";
     $sql = "SELECT * FROM `channels` WHERE
            `channel_id`='" . (int) $_GET['chid'] . "'";
@@ -61,138 +68,151 @@ else
     $channel_sql = '';
 }
 
-if ($category == 'featured')
-{
-    $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_featured`='yes' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql";
-}
-else
-{
-    $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql";
-}
+$view = Cache::load($cache_id);
 
-$result = mysql_query($sql) or mysql_die($sql);
-$video_info = mysql_fetch_assoc($result);
-$total = $video_info['total'];
+if (!$view)
+{
+    
+    if ($category == 'featured')
+    {
+        $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_featured`='yes' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql";
+    }
+    else
+    {
+        $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql";
+    }
+    
+    $result = mysql_query($sql) or mysql_die($sql);
+    $video_info = mysql_fetch_assoc($result);
+    $total = $video_info['total'];
+    
+    $start_from = ($page - 1) * $config['num_watch_videos'];
+    
+    if ($category == 'recent')
+    {
+        $html_title = 'Most Recent Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY `video_add_time` DESC
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    else if ($category == 'viewed')
+    {
+        $html_title = 'Most Viewed Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY `video_view_number` DESC
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    else if ($category == 'discussed')
+    {
+        $html_title = 'Most Discussed Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY `video_com_num` DESC
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    else if ($category == 'favorites')
+    {
+        $html_title = 'Top Favorites Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY `video_fav_num` DESC
+                LIMIT $start_from,$config[num_watch_videos]";
+    }
+    else if ($category == 'rated')
+    {
+        $html_title = 'Top Rated Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY (video_rate/video_rated_by) DESC, video_rated_by DESC
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    else if ($category == 'featured')
+    {
+        $html_title = 'Featured Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql AND
+               `video_featured`='yes'
+                ORDER BY `video_add_time` DESC
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    else if ($category == 'random')
+    {
+        $html_title = 'Random Videos';
+        $sql = "SELECT * FROM `videos` WHERE
+               `video_type`='public' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                $channel_sql
+                ORDER BY rand()
+                LIMIT $start_from, $config[num_watch_videos]";
+    }
+    
+    $result = mysql_query($sql) or mysql_die($sql);
+    $video_count = mysql_num_rows($result);
+    
+    while ($video = mysql_fetch_assoc($result))
+    {
+        $video['video_thumb_url'] = $servers[$video['video_thumb_server_id']];
+        $videos[] = $video;
+    }
+    
+    $start_num = $start_from + 1;
+    $end_num = $start_from + $video_count;
+    
+    $page_links = paginate($total, $config['num_watch_videos'], '.', '', $page);
+    
+    $smarty->assign('page', $page);
+    $smarty->assign('start_num', $start_num);
+    $smarty->assign('end_num', $end_num);
+    $smarty->assign('total', $total);
+    $view['videos'] = $videos;
+    
+    $view['html_title'] = $html_title;
+    $view['html_keywords'] = $html_title;
+    $view['html_description'] = $html_title;
+    Cache::save($cache_id, $view);
+}    
 
-$start_from = ($page - 1) * $config['num_watch_videos'];
+$smarty->assign('html_title', $view['html_title']);
+$smarty->assign('html_keywords', $view['html_title']);
+$smarty->assign('html_description', $view['html_title']);
 
-if ($category == 'recent')
-{
-    $html_title = 'Most Recent Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY `video_add_time` DESC
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-else if ($category == 'viewed')
-{
-    $html_title = 'Most Viewed Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY `video_view_number` DESC
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-else if ($category == 'discussed')
-{
-    $html_title = 'Most Discussed Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY `video_com_num` DESC
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-else if ($category == 'favorites')
-{
-    $html_title = 'Top Favorites Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY `video_fav_num` DESC
-            LIMIT $start_from,$config[num_watch_videos]";
-}
-else if ($category == 'rated')
-{
-    $html_title = 'Top Rated Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY (video_rate/video_rated_by) DESC, video_rated_by DESC
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-else if ($category == 'featured')
-{
-    $html_title = 'Featured Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql AND
-           `video_featured`='yes'
-            ORDER BY `video_add_time` DESC
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-else if ($category == 'random')
-{
-    $html_title = 'Random Videos';
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_type`='public' AND
-           `video_active`='1' AND
-           `video_approve`='1'
-            $channel_sql
-            ORDER BY rand()
-            LIMIT $start_from, $config[num_watch_videos]";
-}
-
-$result = mysql_query($sql) or mysql_die($sql);
-$video_count = mysql_num_rows($result);
-
-while ($video = mysql_fetch_assoc($result))
-{
-    $video['video_thumb_url'] = $servers[$video['video_thumb_server_id']];
-    $videos[] = $video;
-}
-
-$start_num = $start_from + 1;
-$end_num = $start_from + $video_count;
-
-$page_links = paginate($total, $config['num_watch_videos'], '.', '', $page);
-
-$smarty->assign('html_title', $html_title);
-$smarty->assign('html_keywords', $html_title);
-$smarty->assign('html_description', $html_title);
+$smarty->assign('view', $view);
 $smarty->assign('type', $category);
 $smarty->assign('err', $err);
 $smarty->assign('msg', $msg);
-$smarty->assign('page', $page);
-$smarty->assign('start_num', $start_num);
-$smarty->assign('end_num', $end_num);
-$smarty->assign('total', $total);
-$smarty->assign('videos', $videos);
 $smarty->assign('page_links', $page_links);
 $smarty->assign('sub_menu', 'menu_watch.tpl');
 $smarty->display('header.tpl');
-
 $smarty->display('video.tpl');
 $smarty->display('footer.tpl');
 db_close();
