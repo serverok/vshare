86ab9c7a11567f34ba387f500066a4a3e58c3664
unknown <Administrator@.(none)>
2009-06-09 23:03:31 +0530

    test server rocroot change

--- include/config.php
+++ include/config.php
@@ -14,7 +14,7 @@ $config['ffmpeg']          =  '/usr/bin/ffmpeg';
 $config['mplayer']          =  '/usr/bin/mplayer';
 $config['mencoder']          =  '/usr/bin/mencoder';
 $config['flvtool']          =  '/usr/bin/flvtool2';
-$config['basedir']        =  '/home/web/vshare';
+$config['basedir']        =  '/www/web/vshare';
 $config['baseurl']        =  'http://linux/web/vshare';
 
 include($config['basedir'] . '/include/vshare.php');
