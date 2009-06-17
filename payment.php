f473d92dd97662d6d4afcc53fa3a90cec41b5f03
BuyScripts.in <info@buyscripts.in>
2009-06-18 03:08:01 +0530

    added ccbill

--- payment.php
+++ payment.php
@@ -52,6 +52,32 @@ if (isset($_POST['submit']))
            `user_id`='" . (int) $_POST['user_id'] . "'";
     mysql_query($sql) or mysql_die($sql);
     
+    $sql = "CREATE TABLE IF NOT EXISTS `payments` (
+            `payment_id` int(11) unsigned NOT NULL auto_increment,
+            `payment_hash` varchar(255) NOT NULL default '',
+            `payment_user_id` int(10) unsigned NOT NULL default '0',
+            `payment_completed` smallint(1) NOT NULL default '0',
+            `payment_package_id` int(10) unsigned NOT NULL default '0',
+            `payment_period` varchar(255) NOT NULL default '',
+            `payment_amount` varchar(255) NOT NULL default '',
+            PRIMARY KEY  (`payment_id`),
+            KEY `payment_hash` (`payment_hash`)
+            );";
+    mysql_query($sql) or mysql_die($sql);
+    
+    $product_desc = $_POST['user_id'] . '|' . $_POST['package_id'] . '|' . $s_period . '|' . $totalprice;
+    
+    $payment_hash = md5(time());
+    
+    $sql = "INSERT INTO `payments` SET `payment_hash`='" . $payment_hash . "',
+    	   `payment_user_id`='" . (int) $_POST['user_id'] . "',
+    	   `payment_package_id`='" . (int) $_POST['package_id'] . "',
+           `payment_period`='1',
+           `payment_amount`='" . $totalprice . "'";
+    $result = mysql_query($sql);
+    
+    $payment_id = mysql_insert_id();
+    
     if ($_POST['method'] == 'Paypal')
     {
         $s_period = $_POST['period'] . ' ' . $package['package_period'];
@@ -84,6 +110,30 @@ if (isset($_POST['submit']))
         $paypal_link = "https://$url/cgi-bin/webscr/?cmd=_xclick" . "&business=$business" . "&item_number=1&item_name=$item_name" . "&amount=$theprice&on0=0&custom=$uniqueid" . "&currency_code=$config[paypal_currency]" . "&return=$return" . "&cancel_return=$cancel" . "&notify_url=$notify" . "&first_name=$first_name" . "&last_name=$last_name" . "&city=$city";
         
         redirect($paypal_link);
+    }
+    else if ($_POST['method'] == 'CCBill')
+    {
+        $theprice = $totalprice;
+        $s_period = $_POST['period'] . ' ' . $package['package_period'];
+        $product_desc = $_POST['user_id'] . '|' . $_POST['package_id'] . '|' . $s_period . '|' . $totalprice;
+        
+        $return = VSHARE_URL . '/payment/ccbill_success.php';
+        $cancel = VSHARE_URL . '/payment/ccbill_failed.php';
+        $notify = VSHARE_URL . '/payment/ccbill_ipn.php';
+        
+        $return = urlencode($return);
+        $cancel = urlencode($cancel);
+        $notify = urlencode($notify);
+        $first_name = urlencode($_POST['user_first_name']);
+        $last_name = urlencode($_POST['user_last_name']);
+        $city = urlencode($_POST['user_city']);
+        $country = $_POST['user_country'];
+        
+        $client_accnum = $config['ccbill_ac_no'];
+        $client_subacc = $config['ccbill_sub_ac_no'];
+        
+        $ccbill_link = 'https://bill.ccbill.com/jpost/signup.cgi?clientAccnum=' . $client_accnum . '&clientSubacc=' . $client_subacc . '&formName=11cc&customer_fname=' . $first_name . '&customer_lname=' . $last_name . '&city=' . $city . '&country=' . $country . '&accountingAmount=' . $theprice . '&vshare_payment_id=' . $payment_id . '&productDesc=' . $product_desc;
+        redirect($ccbill_link);
     }
 }
 
