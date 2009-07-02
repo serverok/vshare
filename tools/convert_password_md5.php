741190a07cdacc71419022189278b81fbcdbd96b
BuyScripts.in <info@buyscripts.in>
2009-07-03 02:05:48 +0530

    Convert user password to md5 (this is for upgrading version v1 to v2)

---
+++ tools/convert_password_md5.php
@@ -0,0 +1,26 @@
+<?php
+
+require '../include/config.php';
+
+$sql = "SELECT * FROM `users`";
+$result = mysql_query($sql);
+
+while ($userRow = mysql_fetch_assoc($result))
+{
+	$user_name = $userRow['user_name'];
+	$user_id = $userRow['user_id'];
+	$user_password = $userRow['user_password'];
+	if (strlen($user_password) > 30)
+	{
+		echo "<p><b>$user_name = $user_password</b></p>";
+	}
+	else
+	{
+		$user_password_md5 = md5($user_password);
+		$sql = "UPDATE `users` SET 
+			   `user_password`='" . $user_password_md5 . "' WHERE
+			   `user_id`='$user_id'";
+		echo "<p>$sql</p>";
+		$tmp = mysql_query($sql) or die(mysql_error());
+	}
+}
