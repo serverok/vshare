2fcabc74d799aec8786195d5b2cd3394d7362526
unknown <boby@.(none)>
2010-01-01 15:14:45 +0530

    admin log delete

---
+++ admin/admin_log_delete.php
@@ -0,0 +1,49 @@
+<?php
+/******************************************************************************
+ *
+ *   COMPANY: BuyScripts.in
+ *   PROJECT: vShare Youtube Clone
+ *   VERSION: 2.8
+ *   LISENSE: http://buyscripts.in/vshare-license.html
+ *   WEBSITE: http://buyscripts.in/youtube_clone.html
+ *
+ *   This program is a commercial software and any kind of using it must agree 
+ *   to vShare license.
+ *
+ ******************************************************************************/
+
+require '../include/config.php';
+
+check_admin_login();
+
+if (isset($_POST['submit']))
+{
+	for ($i = 0;$i < count($_POST['delete_log']);$i++)
+	{
+		$sql = "DELETE FROM `admin_log` WHERE
+			   `admin_log_id`='" . (int) $_POST['delete_log'][$i] . "'";
+		mysql_query($sql) or mysql_die($sql);
+	}
+}
+
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+
+if (isset($_GET['delete_all']))
+{
+	$sql = "DELETE FROM `admin_log`";
+	mysql_query($sql) or mysql_die($sql);
+	$page = 1;
+}
+
+db_close();
+
+if ($page < 1)
+{
+    $page = 1;
+}
+
+$sort = isset($_GET['sort']) ? $_GET['sort'] : '';
+
+$redirect_url = VSHARE_URL . '/admin/admin_log.php?page=' . $page . '&sort=' . $sort;
+redirect($redirect_url);
+
