ec031ff04dac4540677c4e114f812207b37dbb01
unknown <boby@.(none)>
2010-01-02 14:52:04 +0530

    admin log added

---
+++ admin/admin_log.php
@@ -0,0 +1,79 @@
+<?php
+/******************************************************************************
+ *
+ *   COMPANY: BuyScripts.in
+ *   PROJECT: vShare Youtube Clone
+ *   VERSION: 2.8
+ *   LISENSE: http://buyscripts.in/vshare-license.html
+ *   WEBSITE: http://buyscripts.in/youtube_clone.html
+ *
+ *   This program is a commercial software and any kind of using it must agree 
+ *   to vShare license.
+ *
+ ******************************************************************************/
+
+require '../include/config.php';
+
+check_admin_login();
+
+$admin_listing_per_page = get_config('admin_listing_per_page');
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+
+if ($page < 1)
+{
+    $page = 1;
+}
+
+$sort = isset($_GET['sort']) ? $_GET['sort'] : '';
+
+if ($sort == '')
+{
+    $query = " ORDER BY `admin_log_id` DESC";    
+}
+else
+{
+    $query = " ORDER BY " . mysql_clean($sort);    
+}
+
+//http://linux/web/vshare/admin/admin_log.php?sort=admin_log_1ip+desc&page=1
+
+$sql = "SELECT count(*) AS `total` FROM
+	   `admin_log` $query";
+$result = mysql_query($sql) or mysql_die($sql);
+$tmp = mysql_fetch_assoc($result);
+$total = $tmp['total'];
+
+$start = ($page - 1) * $admin_listing_per_page;
+
+require 'Pager/Pager.php';
+require 'Pager/Sliding.php';
+
+$params = array();
+$params['mode'] = 'Sliding';
+$params['perPage'] = $admin_listing_per_page;
+$params['linkClass'] = 'pager';
+$params['delta'] = 2;
+$params['totalItems'] = $total;
+$params['urlVar'] = 'page';
+
+$pager = new Pager_Sliding($params);
+$data = $pager->getPageData();
+$links = $pager->getLinks();
+
+$sql = "SELECT * FROM `admin_log`
+		$query
+		LIMIT $start, $admin_listing_per_page";
+$result = mysql_query($sql) or mysql_die($sql);
+$admin_log_info = mysql_fetch_all($result);
+
+$smarty->assign('admin_log_info', $admin_log_info);
+$smarty->assign('links', $links["all"]);
+$smarty->assign('page', $page + 0);
+$smarty->assign('sort', $sort);
+
+$smarty->assign('err', $err);
+$smarty->assign('msg', $msg);
+$smarty->display('admin/header.tpl');
+$smarty->display('admin/admin_log.tpl');
+$smarty->display('admin/footer.tpl');
+db_close();
