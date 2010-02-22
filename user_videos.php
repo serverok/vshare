86dddaa9d67559594e3447811ee7bb794b23c340
BizHat.com <info@BizHat.in>
2010-02-23 01:50:04 +0530

    code format

--- user_videos.php
+++ user_videos.php
@@ -1,14 +1,14 @@
 <?php
 /******************************************************************************
  *
- *   COMPANY: BuyScripts.in
- *   PROJECT: vShare Youtube Clone
- *   VERSION: 2.8
- *   LISENSE: http://buyscripts.in/vshare-license.html
- *   WEBSITE: http://buyscripts.in/youtube_clone.html
+ * COMPANY: BuyScripts.in
+ * PROJECT: vShare Youtube Clone
+ * VERSION: 2.8
+ * LISENSE: http://buyscripts.in/vshare-license.html
+ * WEBSITE: http://buyscripts.in/youtube_clone.html
  *
- *   This program is a commercial software and any kind of using it must agree
- *   to vShare license.
+ * This program is a commercial software and any kind of using it must agree
+ * to vShare license.
  *
  ******************************************************************************/
 
@@ -144,15 +144,15 @@ if ($show_video == 1)
     while ($video = mysql_fetch_assoc($result))
     {
         $video['video_thumb_url'] = $servers[$video['video_thumb_server_id']];
-        $video['video_keywords_array'] = explode(' ',$video['video_keywords']);
+        $video['video_keywords_array'] = explode(' ', $video['video_keywords']);
         $video_keywords_all .= $video['video_keywords'] . ' ';
         $videos[] = $video;
     }
     
     $view = array();
-    $video_keywords_array_all = explode(' ',$video_keywords_all);
+    $video_keywords_array_all = explode(' ', $video_keywords_all);
     $view['video_keywords_array_all'] = array_remove_duplicate($video_keywords_array_all);
-        
+    
     if (isset($videos))
     {
         $view['videos'] = $videos;
