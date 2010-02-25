8f244afad09c1c6423ec3934f12610ce815eefe4
jobin <jobin@hostonnet.com>
2010-02-25 16:53:35 +0530

    change code for download youtube video
    http://labs.buyscripts.in/issues/50
    
    Signed-off-by: Boby <info@BizHat.in>

--- include/youtube.php
+++ include/youtube.php
@@ -4,15 +4,28 @@ function get_youtube_flv_url($url)
 {
     $content = file_get_contents($url);
     
-    if (preg_match_all("/fmt_url_map.*/", $content, $matches))
+    if (preg_match('/"video_id": "(.*?)"/', $content, $match) && preg_match('/"t": "(.*?)"/', $content, $match1))
     {
-        $url = urldecode($matches[0][0]);
-        $urls = explode('|', $url);
-        $url = $urls[1];
-        $urls = explode(',', $url);
-        $flv_url = $urls[0];
-        $flv_url = str_replace('"', '', $flv_url);
-        return $flv_url;
+        $url = "http://www.youtube.com/get_video?video_id=" . $match[1] . "&t=" . $match1[1];
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+        $cookie_name = md5(rand() . time());
+        $cookiefile = VSHARE_DIR . '/templates_c/' . $cookie_name;
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookiefile);
+        $data = curl_exec($ch);
+        
+        unlink($cookiefile);
+        
+        if (preg_match('/Location: (.*?)[\r\n]+/', $data, $match) || preg_match('/http-equiv=\'Refresh\' content=\'[0-9]+;url=(.*?)\'/s', $data, $match))
+        {
+            return $match[1];
+        }
     }
     else
     {
