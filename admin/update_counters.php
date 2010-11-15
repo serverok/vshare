24febabf0aa1a8894f2f209dfbf3057c8e0da3e7
BuyScripts <info@buyscripts.in>
2010-11-15 14:42:14 +0530

    update video number of comments

--- admin/update_counters.php
+++ admin/update_counters.php
@@ -16,7 +16,7 @@ require '../include/config.php';
 
 check_admin_login();
 
-$result_per_page = get_config('admin_listing_per_page');
+$result_per_page = 50;
 
 if (isset($_GET['action']))
 {
@@ -30,6 +30,7 @@ if (isset($_GET['action']))
     $items_per_page = isset($_GET['items_per_page']) ? (int) $_GET['items_per_page'] : $result_per_page;
     
     $start = ($page - 1) * $items_per_page;
+    $page ++;
     
     if ($_GET['action'] == 'update_video_counts')
     {
@@ -61,8 +62,6 @@ if (isset($_GET['action']))
                 echo "<p>$user_name = $total videos.</p>";
             }
             
-            $page ++;
-            
             echo '<meta http-equiv="refresh" content="3;url=' . VSHARE_URL . '/admin/update_counters.php?action=update_video_counts&items_per_page=' . $items_per_page . '&page=' . $page . '">';
         }
         else
@@ -72,6 +71,44 @@ if (isset($_GET['action']))
             $redirect_url = VSHARE_URL . '/admin/update_counters.php';
             redirect($redirect_url);
         }
+    }
+    else if ($_GET['action'] == 'update_video_comments_count')
+    {
+        $sql = "SELECT `video_id` FROM `videos` WHERE
+               `video_user_id`!='0' AND
+               `video_active`='1' AND
+               `video_approve`='1'
+                ORDER BY `video_id` ASC
+                LIMIT $start, $items_per_page";
+        $result = mysql_query($sql) or mysql_die($sql);
+        
+        if (mysql_num_rows($result) < 1)
+        {
+            set_message('Video Comments Count Updated Successfully');
+            
+            $redirect_url = VSHARE_URL . '/admin/update_counters.php';
+            redirect($redirect_url);
+        }
+        
+        echo '<strong>Video Comments Count Updating...</strong>';
+        
+        while (list($video_id) = mysql_fetch_array($result))
+        {
+            $sql = "SELECT COUNT(*) AS `total` FROM `comments` WHERE
+                   `comment_video_id`='" . (int) $video_id . "'";
+            $tmp_result = mysql_query($sql) or mysql_die($sql);
+            $tmp_info = mysql_fetch_assoc($tmp_result);
+            $total = $tmp_info['total'];
+            
+            $sql = "UPDATE `videos` SET
+                   `video_com_num`='" . (int) $total . "' WHERE
+                   `video_id`='" . (int) $video_id . "'";
+            mysql_query($sql) or mysql_die($sql);
+            
+            echo "<p>$video_id = $total comments.</p>";
+        }
+        
+        echo '<meta http-equiv="refresh" content="3;url=' . VSHARE_URL . '/admin/update_counters.php?action=update_video_comments_count&items_per_page=' . $items_per_page . '&page=' . $page . '">';
     }
 }
 else
--- templates/admin/update_counters.tpl
+++ templates/admin/update_counters.tpl
@@ -11,7 +11,23 @@
     
     <div class="submit">
         <input type="submit" name="submit" value="Update Video Counts" />
-        <input type="reset" name="reset" value="Reset" />
+    </div>
+    
+</form>
+
+<form method="get" action="">
+    
+    <input type="hidden" name="action" value="update_video_comments_count" />
+    
+    <h1>Update Video Comments Count</h1>
+    <div>This will rebuild comments count of all videos based on the CURRENT comments in the database.</div>
+    <div>
+        <label for="items_per_page">Number of videos to process per cycle:</label>
+        <input type="text" name=items_per_page id="items_per_page" value="{$result_per_page}" />
+    </div>
+    
+    <div class="submit">
+        <input type="submit" name="submit" value="Update Video Comment Counts" />
     </div>
     
 </form>
