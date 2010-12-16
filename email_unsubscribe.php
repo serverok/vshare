0601bccf440aa3707c9c0fccb7b932934e947352
BuyScripts <info@buyscripts.in>
2010-12-17 03:55:09 +0530

    unsubscribe page updated

--- email_unsubscribe.php
+++ email_unsubscribe.php
@@ -13,47 +13,61 @@
  ******************************************************************************/
 
 require 'include/config.php';
+require 'include/language/' . LANG . '/lang_email_unsubscribe.php';
 
 $vkey = isset($_GET['vkey']) ? $_GET['vkey'] : 0;
 $user_name = isset($_GET['user_name']) ? $_GET['user_name'] : '';
+$unsubscribed_success = 0;
 
-if (! empty($vkey) && ! empty($user_name))
+$sql = "SELECT `user_id` FROM `users` WHERE
+	   `user_name`='" . mysql_clean($user_name) . "'";
+$result = mysql_query($sql) or mysql_die($sql);
+
+if (mysql_num_rows($result) != 1)
 {
-    $sql = "SELECT `user_id` FROM `users` WHERE
-           `user_name`='" . mysql_clean($user_name) . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
-    if (mysql_num_rows($result) == 1)
-    {
-        $user_info = mysql_fetch_assoc($result);
-        
-        $data1 = 'UNSUBSCRIBE_' . $user_info['user_id'];
-        
-        $sql = "SELECT * FROM `verify_code` WHERE
-               `vkey`='" . mysql_clean($vkey) . "' AND
-               `data1`='" . mysql_clean($data1) . "'";
-        $result = mysql_query($sql) or mysql_die($sql);
-        
-        if (mysql_num_rows($result) == 1)
-        {
-            $sql = "UPDATE `users` SET `user_subscribe_admin_mail`='0' WHERE
-                   `user_id`='" . $user_info['user_id'] . "'";
-            $result = mysql_query($sql) or mysql_die($sql);
-            
-            $sql = "DELETE FROM `verify_code` WHERE
-                   `vkey`='" . mysql_clean($vkey) . "' AND
-                   `data1`='" . mysql_clean($data1) . "'";
-            $result = mysql_query($sql) or mysql_die($sql);
-            
-            $msg = 'You have successfully unsubscribed.';
-            set_message($msg, 'success');
-            
-            redirect(VSHARE_URL);
-        }
-    }
+		echo $lang['user_not_found'];
+		exit;
 }
 
-$err = 'Invalid authentication code';
-set_message($err, 'error');
+$user_info = mysql_fetch_assoc($result);
 
-redirect(VSHARE_URL);
+$data1 = 'UNSUBSCRIBE_' . $user_info['user_id'];
+
+$sql = "SELECT * FROM `verify_code` WHERE
+	   `vkey`='" . mysql_clean($vkey) . "' AND
+	   `data1`='" . mysql_clean($data1) . "'";
+$result = mysql_query($sql) or mysql_die($sql);
+
+if (mysql_num_rows($result) != 1)
+{
+	echo $lang['invalid_auth_key'];
+	exit;
+}
+				
+if (isset($_POST['submit']))
+{
+        if ($_POST['submit'] == 'Unsubscribe')
+        {
+			$sql = "UPDATE `users` SET `user_subscribe_admin_mail`='0' WHERE
+				   `user_id`='" . $user_info['user_id'] . "'";
+			$result = mysql_query($sql) or mysql_die($sql);
+			
+			$unsubscribe_txt = str_replace('[PRIVACY_SETTINGS_URL]', VSHARE_URL . '/privacy/', $lang['unsubscribed_success']);
+			$unsubscribe_txt = str_replace('[SITE_NAME]', $config['site_name'], $unsubscribe_txt);
+			$smarty->assign('unsubscribe_txt',$unsubscribe_txt);
+			$unsubscribed_success = 1;
+                
+		}
+		else
+		{
+            redirect(VSHARE_URL);
+		}
+}
+
+$smarty->assign('unsubscribed_success', $unsubscribed_success);
+$smarty->assign('err', $err);
+$smarty->assign('msg', $msg);
+$smarty->display('header.tpl');
+$smarty->display('email_unsubscribe.tpl');
+$smarty->display('footer.tpl');
+db_close();
