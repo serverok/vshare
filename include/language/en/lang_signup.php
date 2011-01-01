12e2b35d2b61180f0296feb6b65db63765bb564b
BuyScripts <info@buyscripts.in>
2011-01-01 15:09:54 +0530

    signup age enforce lang file updated
    http://labs.buyscripts.in/issues/214

--- include/language/en/lang_signup.php
+++ include/language/en/lang_signup.php
@@ -19,3 +19,4 @@ $lang['signup_dob_null'] = 'Please enter your date of birth';
 $lang['signup_dob_invalid'] = 'Invalid date of birth';
 $lang['signup_disable'] = 'Signup is disabled by Admin, try later.';
 $lang['signup_verify_email'] = 'Your account has been created. However, this site requires account activation, an activation key has been sent to the e-mail address you provided. Please check your e-mail for further information.';
+$lang['signup_enforce'] = 'You must be at least [AGE_MINIMUM] years old to join.';
