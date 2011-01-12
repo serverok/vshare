76d2807afa65af7ecff1d1e054500abd28a675f5
unknown <boby@.(none)>
2011-01-12 14:53:49 +0530

    fixed import folder error message

--- include/language/en/lang_admin_import_folder_form.php
+++ include/language/en/lang_admin_import_folder_form.php
@@ -6,3 +6,4 @@ $lang['channel_not_selected'] = 'Please check (1 to [NUM_MAX_CHANNELS]) channel/
 $lang['description_too_short'] = 'Please enter a description with minimum 4 characters.';
 $lang['tags_too_short'] = 'Please enter tags with minimum 4 characters.';
 $lang['user_not_found'] = 'User not found.';
+$lang['video_process'] = 'Your video is currently being processed and will be available to view in a few minutes.';
