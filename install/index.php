d4f78b2336ee247be40a02c50efdee7cd67d4fea
BuyScripts <info@buyscripts.in>
2011-05-31 17:49:35 +0530

    title fixed on install

--- install/index.php
+++ install/index.php
@@ -12,21 +12,26 @@
  *
  ******************************************************************************/
 
-$html_title = 'VSHARE UPGRADE';
-require './tpl/header.php';
-
-$vshare_installed = 0;
+$version = '2.8.1';
 
 if (file_exists('../include/config.php') && filesize('../include/config.php') > 0)
 {
    $vshare_installed = 1;
+   $html_title = 'VSHARE UPGRADE';
 }
+else
+{
+	$vshare_installed = 0;
+	$html_title = 'VSHARE INSTALLATION';
+}
+
+require './tpl/header.php';
 
 if ($vshare_installed == 1)
 {
     require '../include/config.php';
 ?>
-    <span class="msg">vShare 2.8.1 Upgrade</span>
+    <span class="msg">vShare <?php echo $version; ?> Upgrade</span>
     
     <div style="display:inline-block;width:550px;"><hr />
         <span style="font-size: 12px;">
@@ -44,7 +49,7 @@ if ($vshare_installed == 1)
 else
 {
 ?>
-    <span class="msg">vShare 2.8.1 Installation</span>
+    <span class="msg">vShare <?php echo $version; ?> Installation</span>
     <div style="display:inline-block;width:550px;"><hr />
         <p>YouTube Clone Script allow you to run your own video sharing portal. Visitors will be able to upload video to your web site, view existing video, comment on video, share video with others.</p>
         <a href="./install.php" class="button">install vshare</a>
