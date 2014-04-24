6f986d387187a31d6b50cf102b6b1644e291a18c
BuyScripts.in <info@buyscripts.in>
2014-04-25 01:42:19 +0530

    mysqli fixed friends

--- friends.php
+++ friends.php
@@ -18,97 +18,76 @@ User::is_logged_in();
 
 $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
 
-if ($page < 1)
-{
+if ($page < 1) {
     $page = 1;
 }
 
 $add_list = isset($_GET['add_list']) ? $_GET['add_list'] : '';
 $add_list = trim($add_list);
 
-if ($add_list != '')
-{
+if ($add_list != '') {
     add_item('users', 'user_friends_type', 'user_id=' . (int) $_SESSION['UID'], $add_list);
     redirect(VSHARE_URL . '/friends/');
 }
 
-if (isset($_GET['del_list']))
-{
+if (isset($_GET['del_list'])) {
     remove_item('users', 'user_friends_type', 'user_id=' . (int) $_SESSION['UID'], $_GET['del_list']);
     $sql = "SELECT `friend_friend_id` FROM `friends` WHERE
            `friend_user_id`='" . (int) $_SESSION['UID'] . "'";
     $result = mysql_query($sql) or mysql_die($sql);
-    while ($tmp = mysql_fetch_assoc($result))
-    {
+    while ($tmp = mysql_fetch_assoc($result)) {
         remove_item('friends', 'friend_type', 'friend_id=' . $tmp['friend_friend_id'], $_GET['del_list']);
     }
     redirect(VSHARE_URL . '/friends/');
 }
 
-if (isset($_POST['action_name']))
-{
-    if ($_POST['action_name'] == 'delete')
-    {
-        while (list($k, $v) = @each($_POST['AID']))
-        {
+if (isset($_POST['action_name'])) {
+    if ($_POST['action_name'] == 'delete') {
+        while (list($k, $v) = @each($_POST['AID'])) {
             $sql = "DELETE FROM `friends` WHERE
                    `friend_id`='" . (int) $v . "'";
             mysql_query($sql) or mysql_die($sql);
         }
         redirect(VSHARE_URL . '/friends/');
-    }
-    else
-    {
+    } else {
         $sql = "SELECT `user_friends_type` FROM `users` WHERE
                `user_id`='" . (int) $_SESSION['UID'] . "'";
-        $result = mysql_query($sql) or mysql_die($sql);
+        $result = DB::fetch1($sql);
+        dd($result);
         $friends_tmp = mysql_fetch_assoc($result);
         $type = explode('|', $friends_tmp['user_friends_type']);
         $cmd = explode('_', $_POST['action_name']);
-        
-        if ($cmd[0] == 'add' && is_numeric($cmd[1]))
-        {
-            while (list($k, $v) = @each($_POST['AID']))
-            {
+
+        if ($cmd[0] == 'add' && is_numeric($cmd[1])) {
+            while (list($k, $v) = @each($_POST['AID'])) {
                 add_item('friends', 'friend_type', 'friend_id=' . (int) $v, $type[$cmd[1]]);
             }
-        }
-        else if ($cmd[0] == 'delete' && is_numeric($cmd[1]))
-        {
-            while (list($k, $v) = @each($_POST['AID']))
-            {
+        } else if ($cmd[0] == 'delete' && is_numeric($cmd[1])) {
+            while (list($k, $v) = @each($_POST['AID'])) {
                 remove_item('friends', 'friend_type', 'friend_id=' . (int) $v, $type[$cmd[1]]);
             }
         }
-        
+
         redirect(VSHARE_URL . '/friends/');
     }
 }
 
-if (isset($_GET['view']) && $_GET['view'] != 'All')
-{
+if (isset($_GET['view']) && $_GET['view'] != 'All') {
     $query = "AND `friend_type` LIKE '%" . mysql_clean($_GET['view']) . "%'";
-}
-else
-{
+} else {
     $query = '';
 }
 
-if (isset($_GET['sort']) && $_GET['sort'] == 'name')
-{
+if (isset($_GET['sort']) && $_GET['sort'] == 'name') {
     $sort = "ORDER BY `friend_name`";
-}
-else
-{
+} else {
     $sort = "ORDER BY `friend_invite_date` DESC";
 }
 
 $sql = "SELECT count(*) AS `total` FROM `friends` WHERE
        `friend_user_id`='" . (int) $_SESSION['UID'] . "'
         $query";
-$result = mysql_query($sql) or mysql_die($sql);
-$tmp = mysql_fetch_assoc($result);
-$total = $tmp['total'];
+$total = DB::getTotal($sql);
 
 $page_links = paginate($total, $config['items_per_page'], '.', '', $page);
 
@@ -120,8 +99,7 @@ $link = "<b>$start - $last of $total</b>&nbsp;&nbsp;";
 $sql = "SELECT DISTINCT * FROM `friends` WHERE
        `friend_user_id`='" . (int) $_SESSION['UID'] . "' $query $sort
         LIMIT $start_from, $config[items_per_page]";
-$result = mysql_query($sql) or mysql_die($sql);
-$friends = mysql_fetch_all($result);
+$friends = DB::fetch($sql);
 $total = count($friends);
 
 $smarty->assign('friends', $friends);
@@ -129,31 +107,25 @@ $smarty->assign('total', $total);
 
 $sql = "SELECT DISTINCT * FROM `friends` WHERE
        `friend_user_id`='" . (int) $_SESSION['UID'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$total_friends = mysql_num_rows($result);
+$result = DB::fetch($sql);
+$total_friends = count($result);
 
 $smarty->assign('total_friends', $total_friends);
 
 $sql = "SELECT `user_friends_type` FROM `users` WHERE
        `user_id`='" . (int) $_SESSION['UID'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$friends_type = mysql_fetch_assoc($result);
+$friends_type = DB::fetch1($sql);
 
 $ftype = explode('|', $friends_type['user_friends_type']);
 sort($ftype);
 
 $ftype_ops = '';
 
-for ($i = 0; $i < count($ftype); $i ++)
-{
-    if ($ftype[$i] != '')
-    {
-        if (isset($_GET['view']) && $_GET['view'] == $ftype[$i])
-        {
+for ($i = 0; $i < count($ftype); $i ++) {
+    if ($ftype[$i] != '') {
+        if (isset($_GET['view']) && $_GET['view'] == $ftype[$i]) {
             $sel = 'selected';
-        }
-        else
-        {
+        } else {
             $sel = '';
         }
         $ftype_ops .= "<option value='" . $ftype[$i] . "' $sel>" . $ftype[$i] . "</option>";
@@ -162,29 +134,20 @@ for ($i = 0; $i < count($ftype); $i ++)
 
 $smarty->assign('ftype_ops', $ftype_ops);
 
-$sql = "SELECT `user_friends_type` FROM `users` WHERE
-       `user_id`='" . (int) $_SESSION['UID'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$friends_type = mysql_fetch_assoc($result);
-
 $type = explode('|', $friends_type['user_friends_type']);
 $type = array_splice($type, 1 - count($type));
 $action_ops = '<option value="" selected="selected">--- Select Action ---</option>' . '<option disabled="disabled">Add to list:</option>';
 
-for ($i = 0; $i < count($type); $i ++)
-{
-    if (trim($type[$i]) != '')
-    {
+for ($i = 0; $i < count($type); $i ++) {
+    if (trim($type[$i]) != '') {
         $action_ops .= '<option value="add_' . ($i + 1) . '">&nbsp;&nbsp;&nbsp;&nbsp;' . $type[$i] . '</option>';
     }
 }
 
 $action_ops .= '<option disabled="disabled">delete from list:</option>';
 
-for ($i = 0; $i < count($type); $i ++)
-{
-    if (trim($type[$i]) != '')
-    {
+for ($i = 0; $i < count($type); $i ++) {
+    if (trim($type[$i]) != '') {
         $action_ops .= "<option value='delete_" . ($i + 1) . "'>&nbsp;&nbsp;&nbsp;&nbsp;$type[$i]</option>";
     }
 }
@@ -199,4 +162,4 @@ $smarty->assign('sub_menu', 'menu_friends.tpl');
 $smarty->display('header.tpl');
 $smarty->display('friends.tpl');
 $smarty->display('footer.tpl');
-db_close();
+DB::close();
