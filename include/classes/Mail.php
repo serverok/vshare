ae0e4986df8d784a2a207cc70cf4ebf4df41d9c4
BuyScripts.in <info@buyscripts.in>
2014-04-25 16:05:01 +0530

    fixed

--- include/classes/Mail.php
+++ include/classes/Mail.php
@@ -2,7 +2,7 @@
 
 class Mail
 {
-    
+
     public $mail = array();
 
     function send($mail)
@@ -15,26 +15,25 @@ class Mail
     {
         require_once VSHARE_DIR . '/include/class.html2text.php';
         require_once 'Zend/Loader.php';
-        
+
         Zend_Loader::loadClass('Zend_Mail');
-        
+
         $html2text = new Html2Text($this->mail['body'], 80);
         $body_text = $html2text->convert();
-        
+
         $email = new Zend_Mail('UTF-8');
         $email->setBodyText($body_text);
         $email->setBodyHtml($this->mail['body']);
         $email->setFrom($this->mail['from_email'], $this->mail['from_name']);
         $email->addTo($this->mail['to_email'], $this->mail['to_name']);
         $email->setSubject($this->mail['subject']);
-        
-        if (isset($this->mail['unsubscribe_url']) && ! empty($this->mail['unsubscribe_url']))
-        {
+
+        if (isset($this->mail['unsubscribe_url']) && ! empty($this->mail['unsubscribe_url'])) {
             $email->addHeader('List-Unsubscribe', $this->mail['unsubscribe_url']);
         }
-        
+
         $email->send();
-    
+
     }
 
 }
--- user_friends_favourites.php
+++ user_friends_favourites.php
@@ -20,31 +20,27 @@ $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 $friends = array();
 $friend_videos = array();
 
-if ($page < 1)
-{
+if ($page < 1) {
     $page = 1;
 }
 
 $sql = "SELECT `friend_friend_id` FROM `friends` WHERE
        `friend_user_id`='" . (int) $_SESSION['UID'] . "' AND
        `friend_status`='Confirmed'";
-$result = mysql_query($sql) or mysql_die();
+$friends_all = DB::fetch($sql);
 
-while ($friend = mysql_fetch_assoc($result))
-{
+foreach ($friends_all as $friend) {
     $friends[] = $friend['friend_friend_id'];
 }
 
 $num_friends = count($friends);
 
-if ($num_friends > 0)
-{
-    
-    if ($friends[0] != '')
-    {
+if ($num_friends) {
+
+    if ($friends[0] != '') {
         $my_friends = implode(',', $friends);
     }
-    
+
     $sql = "SELECT count(*) AS `total` FROM
            `videos` AS v,
            `favourite` AS f WHERE
@@ -53,11 +49,10 @@ if ($num_friends > 0)
     $result = mysql_query($sql) or mysql_die();
     $tmp_result = mysql_fetch_assoc($result);
     $total = $tmp_result['total'];
-    
+
     $start_from = ($page - 1) * $config['items_per_page'];
-    
-    if ($my_friends != '')
-    {
+
+    if ($my_friends != '') {
         $sql = "SELECT * FROM
                `videos` AS v,
                `favourite` AS f WHERE
@@ -68,11 +63,9 @@ if ($num_friends > 0)
                 LIMIT $start_from, $config[items_per_page]";
         $result = mysql_query($sql) or mysql_die();
         $num_result = mysql_num_rows($result);
-        
-        if ($num_result > 0)
-        {
-            while ($friend = mysql_fetch_assoc($result))
-            {
+
+        if ($num_result > 0) {
+            while ($friend = mysql_fetch_assoc($result)) {
                 $friend['video_thumb_url'] = $servers[$friend['video_thumb_server_id']];
                 $friend['video_keywords_array'] = preg_split('/\s+/', $friend['video_keywords']);
                 $friend_videos[] = $friend;
@@ -82,59 +75,51 @@ if ($num_friends > 0)
         $start_num = $start_from + 1;
         $end_num = $start_from + $num_result;
     }
-    
+
     $page_links = paginate($total, $config['items_per_page'], '.', '', $page);
     $smarty->assign('page_links', $page_links);
-    
+
     /*
      * find favorited user name
      */
-    
+
     $favorited_by = array();
-    
-    for ($i = 0; $i < count($favorite_video_id); $i ++)
-    {
+
+    for ($i = 0; $i < count($favorite_video_id); $i ++) {
         $sql = "SELECT f.*,u.user_name FROM
                `favourite` AS f,
                `users` AS u WHERE
                 f.favourite_video_id=" . (int) $favorite_video_id[$i] . " AND
                 f.favourite_user_id=u.user_id";
         $result = mysql_query($sql) or mysql_die($sql);
-        
-        while ($tmp = mysql_fetch_assoc($result))
-        {
-            if (array_key_exists($tmp['favourite_video_id'], $favorited_by))
-            {
+
+        while ($tmp = mysql_fetch_assoc($result)) {
+            if (array_key_exists($tmp['favourite_video_id'], $favorited_by)) {
                 $favorited_by[$tmp['favourite_video_id']] .= ' <a href=' . VSHARE_URL . '/' . $tmp['user_name'] . '>' . $tmp['user_name'] . '</a>';
-            }
-            else
-            {
+            } else {
                 $favorited_by[$tmp['favourite_video_id']] = '<a href=' . VSHARE_URL . '/' . $tmp['user_name'] . '>' . $tmp['user_name'] . '</a>';
             }
-        
         }
     }
-    
+
     $smarty->assign('favorited_by', $favorited_by);
-    
+
     $_REQUEST['UID'] = isset($_REQUEST['UID']) ? $_REQUEST['UID'] : '';
     $_REQUEST['type'] = isset($_REQUEST['type']) ? $_REQUEST['type'] : '';
 }
 
-$smarty->assign('err', $err);
-$smarty->assign('msg', $msg);
-$smarty->assign('page', $page);
-
-if (count($friend_videos) > 0)
-{
+if (count($friend_videos) > 0) {
     $smarty->assign('start_num', $start_num);
     $smarty->assign('end_num', $end_num);
     $smarty->assign('total', $total);
     $smarty->assign('answers', $friend_videos);
 }
 
+$smarty->assign('err', $err);
+$smarty->assign('msg', $msg);
+$smarty->assign('page', $page);
 $smarty->assign('sub_menu', 'menu_friends.tpl');
 $smarty->display('header.tpl');
 $smarty->display('user_friends_favourites.tpl');
 $smarty->display('footer.tpl');
-db_close();
+DB::close();
