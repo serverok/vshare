e7f4ec33dea1b521c71278ee6c5683e0a525ace9
BuyScripts.in <info@buyscripts.in>
2014-04-25 12:30:51 +0530

    fix channel sort.

--- admin/channel_sort.php
+++ admin/channel_sort.php
@@ -19,26 +19,21 @@ check_admin_login();
 
 $result_per_page = get_config('admin_listing_per_page');
 
-if (isset($_POST['submit']))
-{
-    
+if (isset($_POST['submit'])) {
     $id = $_POST['id'];
     $sortorder = $_POST['sortorder'];
-    
-    for ($i = 0; $i < count($id); $i ++)
-    {
+    for ($i = 0; $i < count($id); $i ++) {
         $sql = "UPDATE `channels` SET
                `channel_sort_order`='" . (int) $sortorder[$i] . "'
                 WHERE `channel_id`='" . (int) $id[$i] . "'";
-        mysql_query($sql) or mysql_die($sql);
+        DB::query($sql);
     }
     $smarty->assign('msg', $lang['channel_sort_updated']);
 }
 
 $sort = (isset($_GET['sort'])) ? $_GET['sort'] : 'asc';
 
-if ($sort != 'asc')
-{
+if ($sort != 'asc') {
     $sort = 'desc';
 }
 
@@ -46,15 +41,13 @@ $query = "ORDER BY `channel_sort_order` $sort";
 
 $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
 
-if ($page < 1)
-{
+if ($page < 1) {
     $page = 1;
 }
 
 $sql = "SELECT count(*) AS `total` FROM `channels` $query";
-$result = mysql_query($sql) or mysql_die($sql);
-$tmp = mysql_fetch_assoc($result);
-$total = $tmp['total'];
+$total = DB::getTotal($sql);
+
 $start_from = ($page - 1) * $result_per_page;
 
 require 'Pager/Pager.php';
@@ -75,14 +68,13 @@ $links = $pager->getLinks();
 $sql = "SELECT * FROM `channels`
        $query
        LIMIT $start_from, $result_per_page";
-$result = mysql_query($sql) or mysql_die($sql);
+$channels_all = DB::fetch($sql);
 
 $channels = array();
 
-while ($tmp = mysql_fetch_assoc($result))
-{
-    $tmp['channel_name'] = htmlspecialchars_uni($tmp['channel_name']);
-    $channels[] = $tmp;
+foreach ($channels_all as $channel) {
+    $channel['channel_name'] = htmlspecialchars_uni($channel['channel_name']);
+    $channels[] = $channel;
 }
 
 $smarty->assign('link', $links['all']);
@@ -91,4 +83,4 @@ $smarty->assign('channels', $channels);
 $smarty->display('admin/header.tpl');
 $smarty->display('admin/channel_sort.tpl');
 $smarty->display('admin/footer.tpl');
-db_close();
+DB::close();
