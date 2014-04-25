e5d1dbfd7056b2cd592019f75e198b6035fd74c8
BuyScripts.in <info@buyscripts.in>
2014-04-26 05:17:46 +0530

    updated.

--- show.php
+++ show.php
@@ -15,8 +15,7 @@
 require 'include/config.php';
 require 'include/class.video_player.php';
 
-if (! is_numeric($_GET['id']))
-{
+if (! is_numeric($_GET['id'])) {
     echo "id must be numeric";
     exit(0);
 }
@@ -27,22 +26,18 @@ $sql = "SELECT * FROM `videos` WHERE
        `video_active`='1' AND
        `video_approve`='1' AND
        `video_id`='" . (int) $vid . "'";
-$result = mysql_query($sql) or mysql_die($sql);
+$video_info = DB::fetch1($sql);
 
-if (mysql_num_rows($result) > 0)
-{
-    $video_info = mysql_fetch_assoc($result);
+if ($video_info) {
     $video_id = $vid;
     $video_video_flv_name = $video_info['video_flv_name'];
     $player = new video_player();
     $vshare_player = $player->get_player_code($video_id);
-}
-else
-{
+} else {
     $err = 1;
 }
 
 $smarty->assign('err', $err);
 $smarty->assign('VSHARE_PLAYER', $vshare_player);
 $smarty->display('show.tpl');
-db_close();
+DB::close();
