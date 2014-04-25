6bed931e61d083b0e5ed2887e91d18e65dabe505
BuyScripts.in <info@buyscripts.in>
2014-04-25 13:10:41 +0530

    fix group edit

--- include/classes/Group.php
+++ include/classes/Group.php
@@ -2,21 +2,10 @@
 
 class Group
 {
-    static function getGroupById($group_id)
+    static function getById($group_id)
     {
-        $sql = "SELECT * FROM `groups` WHERE
-               `group_id`='" . (int) $group_id . "'";
-        $result = mysql_query($sql) or mysql_die();
-
-        if (mysql_num_rows($result))
-        {
-            return mysql_fetch_assoc($result);
-        }
-        else
-        {
-            return 0;
-        }
-
+        $sql = 'SELECT * FROM `groups` WHERE
+               `group_id`=' . (int) $group_id ;
+        return DB::fetch1($sql);
     }
-
 }
