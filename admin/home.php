74ecbbe6ae63c1d11e9a8cd5efcb3c57b14b10dc
BuyScripts.in <info@buyscripts.in>
2014-04-25 11:41:15 +0530

    fixed admin home

--- admin/home.php
+++ admin/home.php
@@ -19,98 +19,59 @@ check_admin_login();
 $smarty->assign('vshare_version', $vshare_version);
 
 # number of videos
-
-
 $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
        `video_active`='1'";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_video = $tmp['total'];
-
+$total_video = DB::getTotal($sql);
 $smarty->assign('total_video', $total_video);
 
 # number of public videos
-
-
 $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
        `video_active`='1' AND
        `video_type`='public'";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_public_video = $tmp['total'];
-
+$total_public_video = DB::getTotal($sql);
 $smarty->assign('total_public_video', $total_public_video);
 
 # number of private videos
-
-
 $sql = "SELECT count(*) AS `total` FROM `videos` WHERE
        `video_active`='1' AND
        `video_type`='private'";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_private_video = $tmp['total'];
-
+$total_private_video = DB::getTotal($sql);
 $smarty->assign('total_private_video', $total_private_video);
 
 # number of users
-
-
 $sql = "SELECT count(*) AS `total` FROM `users`";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_users = $tmp['total'];
-
+$total_users = DB::getTotal($sql);
 $smarty->assign('total_users', $total_users);
 
 # number of channels
-
-
 $sql = "SELECT count(*) AS `total` FROM `channels`";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_channel = $tmp['total'];
-
+$total_channel = DB::getTotal($sql);
 $smarty->assign('total_channel', $total_channel);
 
 # number of groups
-
-
 $sql = "SELECT count(*) AS `total` FROM `groups`";
-$result = mysql_query($sql);
-$tmp = mysql_fetch_assoc($result);
-$total_groups = $tmp['total'];
-
+$total_groups = DB::getTotal($sql);
 $smarty->assign('total_groups', $total_groups);
 
 # check version
-
-
 $version_file = VSHARE_DIR . '/templates_c/version.txt';
 
-if (file_exists($version_file))
-{
+if (file_exists($version_file)) {
     $last_check = filemtime($version_file);
     $time_now = $_SERVER['REQUEST_TIME'];
     $time_since_last_chek = ($time_now - $last_check) / (60 * 60);
-    if ($time_since_last_chek > 48)
-    {
+    if ($time_since_last_chek > 48) {
         $check_version_now = 1;
-    }
-    else
-    {
+    } else {
         $check_version_now = 0;
     }
-}
-else
-{
+} else {
     $check_version_now = 1;
 }
 
 $errno = 0;
 
-if ($check_version_now == 1)
-{
+if ($check_version_now == 1) {
     $errstr = '';
     $version_info = '';
     
@@ -130,13 +91,10 @@ unset($version_info);
 
 $version_info = file($version_file);
 
-if (sizeof($version_info) == 2)
-{
+if (sizeof($version_info) == 2) {
     $latest_version = trim($version_info[0]);
     $latest_release = $version_info[1];
-}
-else
-{
+} else {
     $latest_version = 2;
     $latest_release = 2;
 }
@@ -144,18 +102,15 @@ else
 $smarty->assign('latest_version', $latest_version);
 $smarty->assign('latest_release', $latest_release);
 
-if ($errno == 0 && strlen($latest_version) > 2)
-{
-    if ($vshare_version < $latest_version)
-    {
+if ($errno == 0 && strlen($latest_version) > 2) {
+    if ($vshare_version < $latest_version) {
         $smarty->assign('vshare_status', 'old');
-    }
-    else
-    {
+    } else {
         $smarty->assign('vshare_status', 'new');
     }
 }
+
 $smarty->display('admin/header.tpl');
 $smarty->display('admin/home.tpl');
 $smarty->display('admin/footer.tpl');
-db_close();
+DB::close();
