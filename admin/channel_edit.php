c3a034849d062e6203c731fccc9648973324bf04
BuyScripts.in <info@buyscripts.in>
2014-04-25 11:58:05 +0530

    fix channel edit

--- admin/channel_edit.php
+++ admin/channel_edit.php
@@ -20,75 +20,60 @@ check_admin_login();
 
 $result_per_page = get_config('admin_listing_per_page');
 
-if (isset($_POST['edit_channel']))
-{
-    
-    if ($_POST['name'] == '')
-    {
+if (isset($_POST['edit_channel'])) {
+
+    if ($_POST['name'] == '') {
         $err = $lang['channel_name_null'];
-    }
-    else if ($_POST['descrip'] == '')
-    {
+    } else if ($_POST['descrip'] == '') {
         $err = $lang['channel_description_null'];
     }
-    
+
     $_POST['name'] = trim($_POST['name']);
     $seo_name = seo_name($_POST['name']);
-    
+
     $sql = "SELECT * FROM `channels` WHERE
-    	   `channel_seo_name`='" . $seo_name . "' AND
-    	   `channel_id`!='" . (int) $_POST['id'] . "'";
-    $result = mysql_query($sql);
-    
-    if (mysql_num_rows($result))
-    {
+            `channel_seo_name`='" . $seo_name . "' AND
+            `channel_id`!='" . (int) $_POST['id'] . "'";
+    $channel_exisists = DB::fetch1($sql);
+
+    if ($channel_exisists) {
         $err = 'Channel with the name already exists';
     }
-    
+
     $sql = "SELECT * FROM `channels` WHERE
-    	   `channel_name`='" . mysql_clean($_POST['name']) . "' AND
-    	   `channel_id`!='" . (int) $_POST['id'] . "'";
-    $result = mysql_query($sql);
-    
-    if (mysql_num_rows($result))
-    {
+            `channel_name`='" . DB::quote($_POST['name']) . "' AND
+            `channel_id`!='" . (int) $_POST['id'] . "'";
+    $channel_exisists = DB::fetch($sql);
+
+    if ($channel_exisists) {
         $err = 'Channel with the name already exists';
     }
-    
-    if ($err == '')
-    {
-        
+
+    if ($err == '') {
+
         $sql = "UPDATE `channels` SET
-               `channel_name` = '" . mysql_clean($_POST['name']) . "',
-               `channel_seo_name` = '" . mysql_clean($seo_name) . "',
-               `channel_description` = '" . mysql_clean($_POST['descrip']) . "'
+                `channel_name` = '" . DB::quote($_POST['name']) . "',
+                `channel_seo_name` = '" . DB::quote($seo_name) . "',
+                `channel_description` = '" . DB::quote($_POST['descrip']) . "'
                 WHERE `channel_id`='" . (int) $_POST['id'] . "'";
-        $result = mysql_query($sql) or mysql_die($sql);
-        
-        if ($_FILES['picture'] != '')
-        {
+        DB::query($sql);
+
+        if ($_FILES['picture'] != '') {
             $err = upload_jpg($_FILES, 'picture', "{$_POST['id']}.jpg", 120, VSHARE_DIR . '/chimg/');
         }
-        
-        if ($err == '')
-        {
+
+        if ($err == '') {
             set_message($lang['channel_updated'], 'success');
             $redirect_url = VSHARE_URL . '/admin/channel_search.php?id=' . $_POST['id'] . '&action=search';
-            redirect($redirect_url);
+            Http::redirect($redirect_url);
         }
-    }
-    else
-    {
+    } else {
         $_GET['chid'] = $_POST['id'];
     }
 }
 
-if (isset($_GET['chid']) && $_GET['chid'] != '')
-{
-    $sql = "SELECT * FROM `channels` WHERE
-         `channel_id`='" . (int) $_GET['chid'] . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $channel_info = mysql_fetch_assoc($result);
+if (isset($_GET['chid']) && $_GET['chid'] != '') {
+    $channel_info = Channel::getById($_GET['chid']);
     $smarty->assign('channel', $channel_info);
 }
 
@@ -97,4 +82,4 @@ $smarty->assign('msg', $msg);
 $smarty->display('admin/header.tpl');
 $smarty->display('admin/channel_edit.tpl');
 $smarty->display('admin/footer.tpl');
-db_close();
+DB::close();
