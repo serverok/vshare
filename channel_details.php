2a4b4eb133e64a66eaceb935178694dd1361ef07
BuyScripts.in <info@buyscripts.in>
2014-04-25 11:08:15 +0530

    fixed channel details.

--- channel_details.php
+++ channel_details.php
@@ -20,15 +20,12 @@ if (! is_numeric($_GET['id']) || $_GET['id'] < 1)
     redirect($redirect_url);
 }
 
-$sql = "SELECT * FROM `channels` WHERE
-       `channel_id`='" . (int) $_GET['id'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$num_rows = mysql_num_rows($result);
+$channel = Channel::getById($_GET['id']);
 
-if ($num_rows > 0)
-{
+if ($channel) {
+
     $num_channel_videos = get_config('num_channel_video');
-    $channel = mysql_fetch_assoc($result);
+
     $sql = "SELECT count(video_id) AS `total`, `video_user_id` FROM `videos` WHERE
            `video_channels` LIKE '%|" . (int) $_GET['id'] . "|%' AND
            `video_approve`='1' AND
@@ -38,8 +35,7 @@ if ($num_rows > 0)
             GROUP BY `video_user_id`
             ORDER BY `total` DESC
             LIMIT 5";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $most_active_users = mysql_fetch_all($result);
+    $most_active_users = DB::fetch($sql);
     $smarty->assign('most_active_users', $most_active_users);
 
     $sql = "SELECT * FROM `videos` WHERE
@@ -49,19 +45,18 @@ if ($num_rows > 0)
            `video_type`='public'
             ORDER BY `video_add_time` DESC
             LIMIT $num_channel_videos";
-    $result = mysql_query($sql) or mysql_die($sql);
+    $recent_channel_videos_all = DB::fetch($sql);
 
     $recent_channel_videos = array();
 
-    while ($recent_channel_video = mysql_fetch_assoc($result))
-    {
+    foreach ($recent_channel_videos_all AS $recent_channel_video) {
         $recent_channel_video['video_thumb_url'] = $servers[$recent_channel_video['video_thumb_server_id']];
         $recent_channel_videos[] = $recent_channel_video;
     }
+
     $smarty->assign('recent_channel_videos', $recent_channel_videos);
 
     # find popular videos on channel
-
 
     $sql = "SELECT * FROM `videos` WHERE
            `video_channels` LIKE '%|" . (int) $_GET['id'] . "|%' AND
@@ -70,12 +65,12 @@ if ($num_rows > 0)
            `video_type`='public'
             ORDER BY `video_view_number` DESC
             LIMIT $num_channel_videos";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $total = mysql_num_rows($result);
+    $channel_videos_all = DB::fetch($sql);
+    $total = count($recent_channel_videos_all);
 
     $mostview = array();
 
-    while ($video = mysql_fetch_assoc($result))
+    foreach ($channel_videos_all as $video)
     {
         $video['video_thumb_url'] = $servers[$video['video_thumb_server_id']];
         $mostview[] = $video;
@@ -94,4 +89,4 @@ $smarty->assign('msg', $msg);
 $smarty->display('header.tpl');
 $smarty->display('channel_details.tpl');
 $smarty->display('footer.tpl');
-db_close();
+DB::close();
