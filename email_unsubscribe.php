956dfad97f888bc3e3e16ab40843b803165658be
BuyScripts.in <info@buyscripts.in>
2014-04-26 15:31:28 +0530

    fix

--- email_unsubscribe.php
+++ email_unsubscribe.php
@@ -19,49 +19,38 @@ $vkey = isset($_GET['vkey']) ? $_GET['vkey'] : 0;
 $user_name = isset($_GET['user_name']) ? $_GET['user_name'] : '';
 $unsubscribed_success = 0;
 
-$sql = "SELECT `user_id` FROM `users` WHERE
-	   `user_name`='" . mysql_clean($user_name) . "'";
-$result = mysql_query($sql) or mysql_die($sql);
+$user_info = User::getByName($user_name);
 
-if (mysql_num_rows($result) != 1)
-{
-		echo $lang['user_not_found'];
-		exit;
+if (! $user_info) {
+    echo $lang['user_not_found'];
+    exit;
 }
-
-$user_info = mysql_fetch_assoc($result);
 
 $data1 = 'UNSUBSCRIBE_' . $user_info['user_id'];
 
 $sql = "SELECT * FROM `verify_code` WHERE
-	   `vkey`='" . mysql_clean($vkey) . "' AND
-	   `data1`='" . mysql_clean($data1) . "'";
-$result = mysql_query($sql) or mysql_die($sql);
+	   `vkey`='" . DB::quote($vkey) . "' AND
+	   `data1`='" . DB::quote($data1) . "'";
+$verify_info = DB::fetch1($sql);
 
-if (mysql_num_rows($result) != 1)
-{
-	echo $lang['invalid_auth_key'];
-	exit;
+if (! $verify_info) {
+    echo $lang['invalid_auth_key'];
+    exit;
 }
-				
-if (isset($_POST['submit']))
-{
-        if ($_POST['submit'] == 'Unsubscribe')
-        {
-			$sql = "UPDATE `users` SET `user_subscribe_admin_mail`='0' WHERE
-				   `user_id`='" . $user_info['user_id'] . "'";
-			$result = mysql_query($sql) or mysql_die($sql);
-			
-			$unsubscribe_txt = str_replace('[PRIVACY_SETTINGS_URL]', VSHARE_URL . '/privacy/', $lang['unsubscribed_success']);
-			$unsubscribe_txt = str_replace('[SITE_NAME]', $config['site_name'], $unsubscribe_txt);
-			$smarty->assign('unsubscribe_txt',$unsubscribe_txt);
-			$unsubscribed_success = 1;
-                
-		}
-		else
-		{
-            redirect(VSHARE_URL);
-		}
+
+if (isset($_POST['submit'])) {
+    if ($_POST['submit'] == 'Unsubscribe') {
+    	$sql = "UPDATE `users` SET `user_subscribe_admin_mail`='0' WHERE
+    		   `user_id`='" . $user_info['user_id'] . "'";
+    	DB::query($sql);
+
+    	$unsubscribe_txt = str_replace('[PRIVACY_SETTINGS_URL]', VSHARE_URL . '/privacy/', $lang['unsubscribed_success']);
+    	$unsubscribe_txt = str_replace('[SITE_NAME]', $config['site_name'], $unsubscribe_txt);
+    	$smarty->assign('unsubscribe_txt',$unsubscribe_txt);
+    	$unsubscribed_success = 1;
+    } else {
+        Http::redirect(VSHARE_URL);
+    }
 }
 
 $smarty->assign('unsubscribed_success', $unsubscribed_success);
@@ -70,4 +59,4 @@ $smarty->assign('msg', $msg);
 $smarty->display('header.tpl');
 $smarty->display('email_unsubscribe.tpl');
 $smarty->display('footer.tpl');
-db_close();
+DB::close();
