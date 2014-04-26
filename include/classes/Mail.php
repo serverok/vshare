88e4a524078677d3e97321936fc767f0df1fd4b8
BuyScripts.in <info@buyscripts.in>
2014-04-26 13:47:56 +0530

    mail class use local devmail.

--- include/classes/Mail.php
+++ include/classes/Mail.php
@@ -8,7 +8,12 @@ class Mail
     function send($mail)
     {
         $this->mail = $mail;
-        $this->zendMail();
+
+        if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] == 'dev') {
+            $this->devMail();
+        } else {
+            $this->zendMail();
+        }
     }
 
     function zendMail()
@@ -34,6 +39,31 @@ class Mail
 
         $email->send();
 
+    }
+
+    function devMail() {
+
+        $to = $this->mail['to_name'] . ' <' . $this->mail['to_email'] . '>';
+        $from =  $this->mail['from_name'] . ' <'. $this->mail['from_email'] . '>';
+        $subject = $this->mail['subject']
+
+        require_once VSHARE_DIR . '/include/class.html2text.php';
+        $html2text = new Html2Text($this->mail['body'], 80);
+        $body = $html2text->convert();
+
+        $url = 'http://localhost/mail/post.php';
+        $useragent = 'Fast Browser 1.0';
+        $post_data = 'to=' . urlencode($to) . '&' .
+                     'from=' . urlencode($from) . '&' .
+                     'subject=' . urlencode($subject) . '&' .
+                     'body=' . urlencode($body);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
+        $result= curl_exec ($ch);
+        curl_close($ch);
     }
 
 }
