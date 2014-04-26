c77c5622c34aab182c2796db228049deb0a5e955
BuyScripts.in <info@buyscripts.in>
2014-04-26 15:11:24 +0530

    fix

--- admin/user_search.php
+++ admin/user_search.php
@@ -17,72 +17,46 @@ require '../include/language/' . LANG . '/lang_admin_user_search.php';
 
 check_admin_login();
 
-if (isset($_GET['userid']) || isset($_GET['user_name']) || isset($_GET['user_ip']))
-{
+if (isset($_GET['userid']) || isset($_GET['user_name']) || isset($_GET['user_ip'])) {
 
-    if ($_GET['userid'] != null)
-    {
-        if (is_numeric($_GET['userid']))
-        {
-            $sql = "SELECT * FROM `users` WHERE
-                   `user_id`='" . (int) $_GET['userid'] . "'";
-            $result = mysql_query($sql) or mysql_die($sql);
+    if ($_GET['userid'] != null) {
 
-            if (mysql_num_rows($result) == 0)
-            {
+        if (is_numeric($_GET['userid'])) {
+            $user_info = User::getById($_GET['userid']);
+
+            if (! $user_info) {
                 $err = str_replace('[USER_ID]', $_GET['userid'], $lang['userid_not_found']);
-            }
-            else
-            {
+            } else {
                 $redirect_url = VSHARE_URL . '/admin/user_view.php?user_id=' . $_GET['userid'];
-                redirect($redirect_url);
+                Http::redirect($redirect_url);
             }
-        }
-        else
-        {
+        } else {
             $err = $lang['userid_invalid'];
         }
-    }
-    else if ($_GET['user_name'] != null)
-    {
-        $sql = "SELECT `user_id` FROM `users` WHERE
-               `user_name`='" . mysql_clean($_GET['user_name']) . "'";
-        $result = mysql_query($sql) or mysql_die($sql);
-
-        if (mysql_num_rows($result) == 0)
-        {
+    } else if ($_GET['user_name'] != null) {
+        $user_info = User::getByName($_GET['user_name']);
+        if (! $user_info) {
             $err = str_replace('[USERNAME]', $_GET['user_name'], $lang['user_name_not_found']);
+        } else {
+            $redirect_url = VSHARE_URL . '/admin/user_view.php?user_id=' . $user_info['user_id'];
+            Http::redirect($redirect_url);
         }
-        else
-        {
-            $user = mysql_fetch_assoc($result);
-            $redirect_url = VSHARE_URL . '/admin/user_view.php?user_id=' . $user['user_id'];
-            redirect($redirect_url);
-        }
-    }
-    else if ($_GET['user_ip'] != null)
-    {
+    } else if ($_GET['user_ip'] != null) {
         $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 
-        if ($page < 1)
-        {
+        if ($page < 1) {
             $page = 1;
         }
 
-        if (! empty($_GET['sort']))
-        {
-            $query = " ORDER BY " . mysql_clean($_GET['sort']);
-        }
-        else
-        {
+        if (! empty($_GET['sort'])) {
+            $query = " ORDER BY " . DB::quote($_GET['sort']);
+        } else {
             $query = " ORDER BY `user_id` DESC";
         }
 
         $sql = "SELECT count(*) AS `total` FROM `users` WHERE
-               `user_ip`='" . mysql_clean($_GET['user_ip']) . "'";
-        $result = mysql_query($sql) or mysql_die($sql);
-        $tmp = mysql_fetch_array($result);
-        $total = $tmp['total'];
+               `user_ip`='" . DB::quote($_GET['user_ip']) . "'";
+        $total = DB::getTotal($sql);
 
         $admin_listing_per_page = get_config('admin_listing_per_page');
         $start_from = ($page - 1) * $admin_listing_per_page;
@@ -103,11 +77,10 @@ if (isset($_GET['userid']) || isset($_GET['user_name']) || isset($_GET['user_ip'
         $links = $pager->getLinks();
 
         $sql = "SELECT * FROM `users` WHERE
-               `user_ip`='" . mysql_clean($_GET['user_ip']) . "'
+               `user_ip`='" . DB::quote($_GET['user_ip']) . "'
                 $query
                 LIMIT $start_from, $admin_listing_per_page";
-        $result = mysql_query($sql) or mysql_die($sql);
-        $users = mysql_fetch_all($result);
+        $users = DB::fetch($sql);
 
         $smarty->assign('links', $links['all']);
         $smarty->assign('total', $total + 0);
@@ -121,4 +94,4 @@ $smarty->assign('msg', $msg);
 $smarty->display('admin/header.tpl');
 $smarty->display('admin/user_search.tpl');
 $smarty->display('admin/footer.tpl');
-db_close();
+DB::close();
