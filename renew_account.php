a04379d1c8dabd43f0f1f0224021ba7060979181
BuyScripts.in <info@buyscripts.in>
2014-04-28 01:27:07 +0530

    fix

--- renew_account.php
+++ renew_account.php
@@ -15,31 +15,22 @@
 require 'include/config.php';
 require 'include/language/' . LANG . '/lang_renew_account.php';
 
-$_GET['uid'] = (int) $_GET['uid'];
-
-if ($config['enable_package'] == 'yes')
-{
-    if (isset($_POST['submit']))
-    {
+if ($config['enable_package'] == 'yes') {
+    if (isset($_POST['submit'])) {
         $package_id = isset($_POST['package_id']) ? $_POST['package_id'] : '';
-        
-        if ($package_id == '')
-        {
+        if ($package_id == '') {
             $err = $lang['select_package'];
-        }
-        else
-        {
+        } else {
             $redirect_url = VSHARE_URL . '/package_options.php?package_id=' . (int) $package_id . '&user_id=' . $_GET['uid'];
             Http::redirect($redirect_url);
         }
     }
-    
+
     $sql = "SELECT * FROM `packages` WHERE
            `package_status`='Active' AND
            `package_trial`='no'
             ORDER BY `package_price` DESC";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $package = mysql_fetch_all($result);
+    $package = DB::fetch($sql);
     $smarty->assign('package', $package);
 }
 
