e25b1191970347b2693d88e94f997bb336c1183d
BuyScripts.in <info@buyscripts.in>
2014-04-27 14:03:18 +0530

    fix

--- admin/tags_search.php
+++ admin/tags_search.php
@@ -17,54 +17,39 @@ require '../include/language/' . LANG . '/lang_admin_tags_search.php';
 
 check_admin_login();
 
-if (isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
     $search_tag = DB::quote($_POST['search_tag']);
     $sql = "SELECT * FROM `tags` WHERE
            `tag` like '%$search_tag%'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
-    if (mysql_num_rows($result) > 0)
-    {
-        while ($tags = mysql_fetch_assoc($result))
-        {
-            $tag[] = $tags;
-        }
-        $smarty->assign('tag', $tag);
-    }
-    else
-    {
+    $tags_all = DB::fetch($sql);
+
+    if ($tags_all) {
+        $smarty->assign('tag', $tags_all);
+    } else {
         $err = str_replace('[SEARCH_TAG]', $search_tag, $lang['tag_not_found']);
     }
 }
 
-if (isset($_POST['action']))
-{
-    if ($_POST['action'] == 'Disable')
-    {
+if (isset($_POST['action'])) {
+
+    if ($_POST['action'] == 'Disable') {
         $active = 0;
-    }
-    else if ($_POST['action'] == 'Activate')
-    {
+    } else if ($_POST['action'] == 'Activate') {
         $active = 1;
     }
+
     $tag_id = $_POST['action_tag'];
-    
+
     $sql = "UPDATE `tags` SET
            `active`=$active WHERE
            `id`=" . (int) $tag_id;
-    mysql_query($sql) or mysql_die($sql);
+    DB::query($sql);
+
     $msg = 'Tag has been ' . $_POST['action'] . 'd.';
-    
+
     $sql = "SELECT * FROM `tags` WHERE
            `id`='" . (int) $tag_id . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
-    while ($tags = mysql_fetch_assoc($result))
-    {
-        $tag[] = $tags;
-    }
-    
+    $tag = DB::fetch($sql);
     $smarty->assign('tag', $tag);
 }
 
