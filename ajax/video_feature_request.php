e978827f787e9eddf03f51fd1ea23bd714b29dcb
BuyScripts.in <info@buyscripts.in>
2014-04-28 02:19:14 +0530

    fix

--- ajax/video_feature_request.php
+++ ajax/video_feature_request.php
@@ -6,17 +6,13 @@ require '../include/language/' . LANG . '/lang_video_feature_request.php';
 
 $videoId = isset($_POST['vid']) ? $_POST['vid'] : '';
 
-if (! is_numeric($videoId))
-{
+if (! is_numeric($videoId)) {
     $err = $lang['video_id_invalid'];
-}
-else if (! isset($_SESSION['UID']))
-{
+} else if (! isset($_SESSION['UID'])) {
     $err = $lang['guest'];
 }
 
-if ($err != '')
-{
+if ($err != '') {
     if ($config['debug']) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_video_feature_log.txt');
     return_json($err, 'error');
     exit();
@@ -24,25 +20,21 @@ if ($err != '')
 
 $sql = "SELECT count(*) AS `total` FROM `feature_requests` WHERE
        `feature_request_video_id`='" . (int) $videoId . "'";
-$result = mysql_query($sql) or musql_die($sql);
-$tmp = mysql_fetch_assoc($result);
+$already_requested = DB::getTotal($sql);
 
-if ($tmp['total'] > 0)
-{
+if ($already_requested) {
     $sql = "UPDATE `feature_requests` SET
            `feature_request_count`=feature_request_count + 1,
            `feature_request_date`='" . date("Y-m-d") . "' WHERE
            `feature_request_video_id`='" . (int) $videoId . "'";
-}
-else
-{
+} else {
     $sql = "INSERT `feature_requests` SET
            `feature_request_video_id`=" . (int) $videoId . ",
            `feature_request_count`=1,
            `feature_request_date`='" . date("Y-m-d") . "'";
 }
 
-$result = mysql_query($sql) or mysql_die($sql);
+DB::query($sql);
 
 if ($config['debug']) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_video_feature_log.txt');
 return_json($lang['feature_request_ok'], 'success');
