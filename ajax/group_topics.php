0dd28e537b3e541368947b721ab4e448db3ac3a0
BuyScripts.in <info@buyscripts.in>
2014-04-28 02:13:25 +0530

    fix

--- ajax/group_topics.php
+++ ajax/group_topics.php
@@ -6,8 +6,7 @@ require '../include/language/' . LANG . '/lang_group_topics.php';
 
 $ajax_debug = 0;
 
-if (! isset($_SESSION['UID']))
-{
+if (! isset($_SESSION['UID'])) {
     $err = 'Please Login to post new topic';
     if ($ajax_debug) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
     return_json($err, 'error');
@@ -16,52 +15,39 @@ if (! isset($_SESSION['UID']))
 
 $video_insert = 1;
 
-if (isset($_POST['add_topic']))
-{
+if (isset($_POST['add_topic'])) {
     $group_id = isset($_POST['group_id']) ? (int) $_POST['group_id'] : 0;
     $topic_video = isset($_POST['topic_video']) ? (int) $_POST['topic_video'] : '';
     $topic_title = isset($_POST['topic_title']) ? $_POST['topic_title'] : '';
     $topic_title = htmlspecialchars_uni($topic_title);
-    
-    $sql = "SELECT * FROM 
+
+    $sql = "SELECT * FROM
            `groups` AS `gs`,
-           `group_members` AS `gm` WHERE 
-            gs.group_id='" . (int) $group_id . "' AND 
-            gm.group_member_user_id='" . (int) $_SESSION['UID'] . "' AND 
+           `group_members` AS `gm` WHERE
+            gs.group_id='" . (int) $group_id . "' AND
+            gm.group_member_user_id='" . (int) $_SESSION['UID'] . "' AND
             gm.group_member_approved='yes'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
-    if (mysql_num_rows($result) > 0)
-    {
-        $group_info = mysql_fetch_assoc($result);
-        
-        if (mb_strlen($topic_title, 'UTF-8') > 4)
-        {
-            if ($_SESSION['UID'] == $group_info['group_owner_id'])
-            {
+    $groups_info = DB::fetch1($sql);
+
+    if ($groups_info) {
+        if (mb_strlen($topic_title, 'UTF-8') > 4) {
+            if ($_SESSION['UID'] == $group_info['group_owner_id']) {
                 $approved = 'yes';
-            }
-            else if ($group_info['group_posting'] == 'owner_only')
-            {
+            } else if ($group_info['group_posting'] == 'owner_only') {
                 $err = $lang['topic_add_owner_only'];
                 if ($ajax_debug) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
                 return_json($err, 'error');
                 exit();
-            }
-            else if ($group_info['group_posting'] == 'owner_approve')
-            {
+            } else if ($group_info['group_posting'] == 'owner_approve') {
                 $approved = 'no';
-            }
-            else
-            {
+            } else {
                 $approved = 'yes';
             }
-            
-            if ($topic_video < 1)
-            {
+
+            if ($topic_video < 1) {
                 $topic_video = '';
             }
-            
+
             $sql = "INSERT INTO `group_topics` SET
                    `group_topic_group_id`='" . (int) $group_id . "',
                    `group_topic_user_id`='" . (int) $_SESSION['UID'] . "',
@@ -69,75 +55,56 @@ if (isset($_POST['add_topic']))
                    `group_topic_title`='" . DB::quote($topic_title) . "',
                    `group_topic_video_id`='$topic_video',
                    `group_topic_approved`='$approved'";
-            
-            $result = mysql_query($sql) or mysql_die($sql);
-            
-            if ($topic_video != '')
-            {
-                if ($_SESSION['UID'] == $group_info['group_owner_id'])
-                {
+
+            DB::query($sql);
+
+            if ($topic_video != '') {
+                if ($_SESSION['UID'] == $group_info['group_owner_id']) {
                     $video_approved = 'yes';
-                }
-                else if ($group_info['group_upload'] == 'owner_approve')
-                {
+                } else if ($group_info['group_upload'] == 'owner_approve') {
                     $video_approved = 'no';
-                }
-                else if ($group_info['group_upload'] == 'owner_only' && $_SESSION['UID'] != $group_info['group_owner_id'])
-                {
+                } else if ($group_info['group_upload'] == 'owner_only' && $_SESSION['UID'] != $group_info['group_owner_id']) {
                     $video_insert = 0;
-                }
-                else
-                {
+                } else {
                     $video_approved = 'yes';
                 }
-                
-                if ($video_insert == 1)
-                {
+
+                if ($video_insert == 1) {
                     $sql = "SELECT * FROM `group_videos` WHERE
                            `group_video_group_id`='" . (int) $group_id . "' AND
                            `group_video_video_id`='$topic_video'";
-                    $result = mysql_query($sql) or mysql_die($sql);
-                    
-                    if (mysql_num_rows($result) < 1)
-                    {
+                    $group_videos = DB::fetch($sql);
+
+                    if (! $group_videos) {
                         $sql = "INSERT INTO `group_videos` SET
                                `group_video_group_id`='" . (int) $group_id . "',
                                `group_video_video_id`='$topic_video',
                                `group_video_member_id`='" . (int) $_SESSION['UID'] . "',
                                `group_video_approved`='$video_approved'";
-                        mysql_query($sql) or mysql_die($sql);
-                    
+                        DB::query($sql);
                     }
                 }
             }
-            
-            if ($approved == 'yes')
-            {
+
+            if ($approved == 'yes') {
                 $msg = $lang['topic_add_success'];
                 if ($ajax_debug) error_log("$msg \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
                 return_json($msg, 'success');
                 exit();
-            }
-            else
-            {
+            } else {
                 $msg = $lang['topic_approval_required'];
                 if ($ajax_debug) error_log("$msg \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
                 return_json($msg, 'success');
                 exit();
             }
-        
-        }
-        else
-        {
+        } else {
             $err = $lang['topic_add_security_error'];
             if ($ajax_debug) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
             return_json($err, 'error');
             DB::close();
             exit();
         }
-    }
-    else
-    {
+    } else {
         $err = $lang['topic_add_security_error'];
         if ($ajax_debug) error_log("$err \n", 3, VSHARE_DIR . '/templates_c/ajax_log.txt');
         return_json($err, 'error');
@@ -145,8 +112,3 @@ if (isset($_POST['add_topic']))
         exit();
     }
 }
-
-
-
-
-
