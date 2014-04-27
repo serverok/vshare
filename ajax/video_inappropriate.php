40972f8c68bd7a39a9b1c5bf00c8478a2b00276d
BuyScripts.in <info@buyscripts.in>
2014-04-28 02:16:46 +0530

    fix

--- ajax/video_inappropriate.php
+++ ajax/video_inappropriate.php
@@ -7,13 +7,12 @@
  *   LISENSE: http://buyscripts.in/vshare-license.html
  *   WEBSITE: http://buyscripts.in/youtube_clone.html
  *
- *   This program is a commercial software and any kind of using it must agree 
+ *   This program is a commercial software and any kind of using it must agree
  *   to vShare license.
  *
  ******************************************************************************/
 
 require '../include/config.php';
-require '../include/class.mail.php';
 require '../include/functions_ajax.php';
 require '../include/language/' . LANG . '/lang_video_inappropriate.php';
 
@@ -50,49 +49,44 @@ if ($err != '')
     exit();
 }
 
-$sql = "SELECT count(*) AS `total` FROM `inappropriate_requests` WHERE 
+$sql = "SELECT count(*) AS `total` FROM `inappropriate_requests` WHERE
        `inappropriate_request_video_id`='" . (int) $video_id . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$tmp = mysql_fetch_assoc($result);
+$already_reported = DB::getTotal($sql);
 
-if ($tmp['total'] > 0)
-{
-    $sql = "UPDATE `inappropriate_requests` SET 
+if ($already_reported) {
+    $sql = "UPDATE `inappropriate_requests` SET
            `inappropriate_request_count`=`inappropriate_request_count`+1,
-           `inappropriate_request_date`='" . date("Y-m-d") . "' WHERE 
+           `inappropriate_request_date`='" . date("Y-m-d") . "' WHERE
            `inappropriate_request_video_id`=" . (int) $video_id;
-}
-else
-{
-    $sql = "INSERT `inappropriate_requests` SET 
+} else {
+    $sql = "INSERT `inappropriate_requests` SET
            `inappropriate_request_video_id`='" . (int) $video_id . "',
            `inappropriate_request_count`=1,
            `inappropriate_request_date`='" . date("Y-m-d") . "'";
 }
 
-$result = mysql_query($sql) or mysql_die($sql);
+DB::query($sql);
 
 $mail_abuse_report = get_config('mail_abuse_report');
 
-if ($mail_abuse_report == 1)
-{
-    $sql = "SELECT `video_id`,`video_title`,`video_seo_name` FROM `videos` WHERE 
+if ($mail_abuse_report) {
+
+    $sql = "SELECT `video_id`,`video_title`,`video_seo_name` FROM `videos` WHERE
            `video_id`='" . (int) $video_id . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $video_info = mysql_fetch_assoc($result);
-    
-    $sql = "SELECT * FROM `email_templates` WHERE 
+    $video_info = DB::fetch1($sql);
+
+    $sql = "SELECT * FROM `email_templates` WHERE
            `email_id`='abuse_report'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $tmp = mysql_fetch_assoc($result);
+    $tmp = DB::fetch1($sql);
+
     $email_body = $tmp['email_body'];
     $email_subject = $tmp['email_subject'];
-    
+
     $user_name = isset($_SESSION['USERNAME']) ? $_SESSION['USERNAME'] : 'Guest';
     $video_url = VSHARE_URL . '/view/' . $video_id . '/' . $video_info['video_seo_name'] . '/';
-    
+
     $email_subject = str_replace('[VIDEO_TITLE]', $video_info['video_title'], $email_subject);
-    
+
     $email_body = str_replace('[SITE_NAME]', $config['site_name'], $email_body);
     $email_body = str_replace('[SITE_URL]', VSHARE_URL, $email_body);
     $email_body = str_replace('[USER_NAME]', $user_name, $email_body);
@@ -103,7 +97,7 @@ if ($mail_abuse_report == 1)
     $email_body = str_replace('[TYPE_ABUSE]', $abuse_type, $email_body);
     $email_body = str_replace('[ABUSE_COMMENTS]', nl2br(stripcslashes($comment)), $email_body);
     $email_body = str_replace('[REMOTE_ADDR]', $_SERVER['REMOTE_ADDR'], $email_body);
-    
+
     $msg = array();
     $msg['from_email'] = $config['admin_email'];
     $msg['from_name'] = $config['site_name'];
