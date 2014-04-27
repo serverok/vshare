db445eefb0b5189a73070debf204455d83602aa4
BuyScripts.in <info@buyscripts.in>
2014-04-28 03:47:17 +0530

    fix

--- group_invite_confirm.php
+++ group_invite_confirm.php
@@ -21,59 +21,55 @@ $group_url = htmlspecialchars_uni($_GET['group_url']);
 
 $sql = "SELECT * FROM `groups` WHERE
        `group_url`='" . DB::quote($group_url) . "'";
-$result = mysql_query($sql) or mysql_die($sql);
+$group_info = DB::fetch1($sql);
 
-if (mysql_num_rows($result) == 0)
-{
+if (! $group_info) {
     Http::redirect(VSHARE_URL);
 }
 
-if (! isset($_GET['key']))
-{
+if (! isset($_GET['key'])) {
     DB::close();
     Http::redirect(VSHARE_URL);
 }
 
-$sql = "SELECT * FROM
-       `verify_code` AS v,
+$sql = "SELECT * FROM `verify_code` AS v,
        `groups` AS g WHERE
         v.vkey='" . DB::quote($_GET['key']) . "' AND
         v.data1=g.group_id";
-$result = mysql_query($sql) or mysql_die($sql);
+$verify_info = DB::fetch1($sql);
 
-if (mysql_num_rows($result) < 1)
-{
+if (! $verify_info) {
     DB::close();
     set_message($lang['invalid_invite_key'], 'error');
     $redirect_url = VSHARE_URL . '/group/' . $_GET['group_url'] . '/';
     Http::redirect($redirect_url);
 }
 
-$tmp = mysql_fetch_assoc($result);
-$join_group_id = $tmp['group_id'];
+$join_group_id = $group_info['group_id'];
 
 $sql = "SELECT * FROM `group_members` WHERE
        `group_member_group_id`='" . (int) $join_group_id . "' AND
        `group_member_user_id`='" . (int) $_SESSION['UID'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
+$is_member = DB::fetch1($sql);
 
-if (mysql_num_rows($result) < 1)
-{
+if (! $is_member) {
     $sql = "INSERT INTO `group_members` SET
            `group_member_group_id`='" . (int) $join_group_id . "',
            `group_member_user_id`='" . (int) $_SESSION['UID'] . "',
            `group_member_since`='" . date("Y-m-d H:i:s") . "',
            `group_member_approved`='yes'";
-    $result = mysql_query($sql) or mysql_die($sql);
+    DB::query($sql);
     $msg = $lang['user_added'];
 }
 
 $sql = "DELETE FROM `verify_code` WHERE
        `vkey`='" . DB::quote($_GET['key']) . "'";
-mysql_query($sql) or mysql_die($sql);
+DB::query($sql);
+DB::close();
+
 $smarty->assign('accept_mem', 'true');
 
-DB::close();
 set_message($msg, 'success');
+
 $redirect_url = VSHARE_URL . '/group/' . $_GET['group_url'] . '/';
 Http::redirect($redirect_url);
