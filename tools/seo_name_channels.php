2d94f52ea9a5f4a6fca741d14347302367fc0c4b
BuyScripts.in <info@buyscripts.in>
2014-04-28 03:32:49 +0530

    fix

--- tools/seo_name_channels.php
+++ tools/seo_name_channels.php
@@ -1,18 +1,15 @@
 <?php
 
-include('../include/config.php');
-include('../include/functions_seo_name.php');
+require '../include/config.php';
+require '../include/functions_seo_name.php';
 
 $sql = "SELECT * FROM `channel`";
-$result = mysql_query($sql) or mysql_die($sql);
+$channels_all = DB::fetch($sql);
 
-while($channel = mysql_fetch_assoc($result)) {
+foreach ($channels_all as $channel) {
 	$seo_name = seo_name($channel['channel_name']);
-	$sql = "UPDATE `channels` SET 
-           `channel_seo_name`='$seo_name' WHERE 
+	$sql = "UPDATE `channels` SET
+           `channel_seo_name`='$seo_name' WHERE
            `channel_id`='" . $channel['channel_id'] ."'";
-	mysql_query($sql) or mysql_die($sql);
+	DB::query($sql);
 }
-
-
-?>
