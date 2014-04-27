187d058a14d1e59bbf8a7397f315796fe9fa8331
BuyScripts.in <info@buyscripts.in>
2014-04-27 13:07:33 +0530

    fix

--- user_photo_upload.php
+++ user_photo_upload.php
@@ -17,33 +17,24 @@ require 'include/language/' . LANG . '/lang_user_photo_upload.php';
 
 User::is_logged_in();
 
-if (isset($_FILES['photo']['tmp_name']) && is_uploaded_file($_FILES['photo']['tmp_name']))
-{
+$user_info = User::getById($_SESSION['UID']);
+
+if (isset($_FILES['photo']['tmp_name']) && is_uploaded_file($_FILES['photo']['tmp_name'])) {
     $file_type = $_FILES['photo']['type'];
     $file_tmp_name = $_FILES['photo']['tmp_name'];
-    
-    if (($file_type == 'image/jpeg') || ($file_type == 'image/pjpeg'))
-    {
+    if (($file_type == 'image/jpeg') || ($file_type == 'image/pjpeg')) {
         $image_size = getimagesize($file_tmp_name);
-        
-        if ($image_size[2] == 2)
-        {
+        if ($image_size[2] == 2) {
             User::upload_photo();
             $msg = $lang['photo_uploaded'];
         }
-    }
-    else
-    {
+    } else {
         $err = str_replace('[FILE_TYPE]', $file_type, $lang['invalid_file']);
     }
 }
 
-$sql = "SELECT `user_id`, `user_photo` FROM `users` WHERE
-       `user_id`='" . (int) $_SESSION['UID'] . "'";
-$result = mysql_query($sql) or mysql_die($sql);
-$user_info = mysql_fetch_assoc($result);
-
 $photo_url = User::get_photo($user_info['user_photo'], $user_info['user_id']);
+
 $smarty->assign('vshare_rand', $_SERVER['REQUEST_TIME']);
 $smarty->assign('photo_url', $photo_url);
 $smarty->assign('uid', $_SESSION['UID']);
