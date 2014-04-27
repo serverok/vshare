5b36fbfc8ce60b59f5dbe327529fb706a13d6376
BuyScripts.in <info@buyscripts.in>
2014-04-28 03:36:02 +0530

    fix

--- include/functions_video_thumb.php
+++ include/functions_video_thumb.php
@@ -4,103 +4,81 @@
 function create_video_thumb($video_id)
 {
     global $config;
-$config['debug'] = 0;
+    $config['debug'] = 0;
+
     $log_file_name = 'create_thumb_' . $video_id;
-    
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_id`='" . (int) $video_id . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
+
     echo "<p>Inside finction " . $video_id . "</p>";
-    
-    if (mysql_num_rows($result) < 1)
-    {
+
+    $video_info = Video::getById($video_id);
+
+    if (! $video_info) {
         echo '<p>No video found</p>';
         return 'Video not found.';
     }
-    
-    $video_info = mysql_fetch_assoc($result);
-    
+
     echo '<pre>';
     print_r($video_info);
     echo '</pre>';
-    
+
     $video_src = VSHARE_DIR . '/video/' . $video_info['video_name'];
-    
-    if ($config['debug'])
-    {
+
+    if ($config['debug']) {
         $log_text = '<p>$video_src = ' . $video_src . '</p>';
         write_log($log_text, $log_file_name, $config['debug'], 'html');
     }
-    
-    if (file_exists($video_src) && is_file($video_src))
-    {
-        if ($config['debug'])
-        {
+
+    if (file_exists($video_src) && is_file($video_src)) {
+        if ($config['debug']) {
             $log_text = '<p>File found = ' . $video_src . '</p>';
             write_log($log_text, $log_file_name, $config['debug'], 'html');
         }
-        
-        if ($video_info['video_folder'] != '')
-        {
-            if (! is_dir(VSHARE_DIR . '/thumb/' . $video_info['video_folder']))
-            {
+
+        if ($video_info['video_folder'] != '') {
+            if (! is_dir(VSHARE_DIR . '/thumb/' . $video_info['video_folder'])) {
                 mkdir(VSHARE_DIR . '/thumb/' . $video_info['video_folder']);
             }
         }
-        
-        if ($config['debug'])
-        {
+
+        if ($config['debug']) {
             $log_text = '<p>thumb_folder = ' . VSHARE_DIR . '/thumb/' . $video_info['video_folder'] . '</p>';
             write_log($log_text, $log_file_name, $config['debug'], 'html');
         }
-        
+
         $t_info = array();
         $t_info['src'] = $video_src;
         $t_info['vid'] = (int) $video_id;
         $t_info['video_folder'] = $video_info['video_folder'];
         $t_info['debug'] = $config['debug'];
-        
+
         $video_duration_cmd = get_config('video_duration_cmd');
-        
-        if ($video_duration_cmd == 0)
-        {
+
+        if ($video_duration_cmd == 0) {
             $duration = video_duration::find_video_duration_mplayer($t_info);
             $find_with = 'mplayer';
-        }
-        else if ($video_duration_cmd == 1)
-        {
+        } else if ($video_duration_cmd == 1) {
             $duration = video_duration::find_video_duration_ffmpeg($t_info);
             $find_with = 'ffmpeg';
-        }
-        else
-        {
+        } else {
             $duration = video_duration::find_video_duration_ffmpeg_php($t_info);
             $find_with = 'ffmpeg-php';
         }
-        
+
         $t_info['duration'] = $duration;
-        
-        if ($video_duration_cmd == 0)
-        {
+
+        if ($video_duration_cmd == 0) {
             $tmp = video_thumb::create_thumb_mplayer($t_info);
             $find_with = 'mplayer';
-        }
-        else if ($video_duration_cmd == 1)
-        {
+        } else if ($video_duration_cmd == 1) {
             $tmp = video_thumb::create_thumb_ffmpeg($t_info);
             $find_with = 'ffmpeg';
-        }
-        else
-        {
+        } else {
             $tmp = video_thumb::create_thumb_ffmpeg_php($t_info);
             $find_with = 'ffmpeg-php';
         }
-        
-        if ($video_info['video_thumb_server_id'] > 0)
-        {
-            if ($config['debug'])
-            {
+
+        if ($video_info['video_thumb_server_id'] > 0) {
+            if ($config['debug']) {
                 $log_text = '<p>$video_info[\'video_thumb_server_id\'] = ' . $video_info['video_thumb_server_id'] . '</p>';
                 write_log($log_text, $log_file_name, $config['debug'], 'html');
             }
@@ -112,11 +90,8 @@ $config['debug'] = 0;
             $ftp = new Ftp();
             $ftp->upload_thumb($ftp_config);
         }
-
         echo '<p>Thumbnail created</p>';
-    }
-    else
-    {
+    } else {
         echo '<p>Video not found</p>';
     }
 }
