0507e2690edba2aa17d66d582d9001a916ab7267
BuyScripts.in <info@buyscripts.in>
2014-04-27 13:05:02 +0530

    fix

--- video_embed.php
+++ video_embed.php
@@ -16,31 +16,24 @@ require 'include/config.php';
 
 $video_id = isset($_GET['video_id']) ? (int) $_GET['video_id'] : 0;
 
-if ($video_id == 0)
-{
+if ($video_id == 0) {
     echo "invalid video id";
     exit();
 }
 
-$sql = "SELECT * FROM `videos` WHERE
-       `video_id`='$video_id'";
-$result = mysql_query($sql) or mysql_die($sql);
-$video_info = mysql_fetch_assoc($result);
+$video_info = Video::getById($video_id);
 
 $video_thumb_url = $servers[$video_info['video_thumb_server_id']];
 $logo = IMG_CSS_URL . '/images/watermark.gif';
 $image = $video_thumb_url . '/thumb/' . $video_info['video_folder'] . '/' . $video_id . '.jpg';
 $vshare_player = get_config('vshare_player');
 
-if ($vshare_player == 'StrobeMediaPlayback')
-{
+if ($vshare_player == 'StrobeMediaPlayback') {
     $file_url = 'src=' . VSHARE_URL . '/flvideo/' . $video_info['video_folder'] . $video_info['video_flv_name'];
     $video_flv_player = VSHARE_URL . '/player/player_adobe.swf?';
     $poster = '&poster=' . $image;
     $video_flv_player .= $file_url . $poster;
-}
-else
-{
+} else {
     $file_url = 'file=' . VSHARE_URL . '/xml_playlist.php?id=' . $video_info['video_id'];
     $video_flv_player = VSHARE_URL . '/player/player.swf?';
     $video_flv_player .= $file_url;
@@ -48,7 +41,7 @@ else
 }
 
 $sql = "UPDATE `videos` SET `video_view_number`=`video_view_number`+1 WHERE `video_id`=$video_id";
-$result = mysql_query($sql);
+$result = DB::query($sql);
 
 header('Content-Type: video/flv');
 Header("Location: $video_flv_player");
