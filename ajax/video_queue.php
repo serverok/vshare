0de912058b653e94a98159bdf15b3adac2cec6d4
BuyScripts.in <info@buyscripts.in>
2014-04-28 01:39:03 +0530

    fix

--- ajax/video_queue.php
+++ ajax/video_queue.php
@@ -4,27 +4,16 @@ require '../include/config.php';
 
 $video_id = isset($_GET['video_id']) ? (int) $_GET['video_id'] : 0;
 
-$sql = "SELECT * FROM `videos` WHERE
-       `video_id`=" . $video_id;
-$result = mysql_query($sql) or mysql_die($sql);
+$video_info = Video::getById($video_id);
 
-if (mysql_num_rows($result) > 0)
-{
-    if (isset($_COOKIE['video_queue']) && !empty($_COOKIE['video_queue']))
-    {
-      
+if ($video_info) {
+    if (isset($_COOKIE['video_queue']) && !empty($_COOKIE['video_queue'])) {
         $tmp = explode(',',$_COOKIE['video_queue']);
-
-        if (!in_array($video_id,$tmp))
-        {
+        if (!in_array($video_id,$tmp)) {
             $video_ids = $_COOKIE['video_queue'] .  $video_id . ',' ;
             setcookie('video_queue', $video_ids, time() + 86400,'/');
         }
-        
-    }
-    else
-    {
+    } else {
         setcookie('video_queue', $video_id . ',', time() + 86400,'/');
     }
 }
-
