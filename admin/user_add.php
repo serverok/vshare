e492ae69bfc33c09399f03ed287cb885e1152ea8
BuyScripts.in <info@buyscripts.in>
2014-04-27 16:17:19 +0530

    fix

--- admin/user_add.php
+++ admin/user_add.php
@@ -19,38 +19,26 @@ require '../include/language/' . LANG . '/lang_admin_user_add.php';
 
 check_admin_login();
 
-if (isset($_POST['submit']))
-{
-    if (strlen($_POST['user_name']) < 4)
-    {
+if (isset($_POST['submit'])) {
+    if (strlen($_POST['user_name']) < 4) {
         $err = $lang['user_name_short'];
-    }
-    else if (disallow_user_names($_POST['user_name']))
-    {
+    } else if (disallow_user_names($_POST['user_name'])) {
         $err = $lang['user_name_reserved'];
-    }
-    else if (check_field_exists($_POST['user_name'], 'user_name', 'users') == 1)
-    {
+    } else if (check_field_exists($_POST['user_name'], 'user_name', 'users') == 1) {
         $err = $lang['user_name_exist'];
-    }
-    else if (empty($_POST['user_email']) || ! validate::email($_POST['user_email']))
-    {
+    } else if (empty($_POST['user_email']) || ! validate::email($_POST['user_email'])) {
         $err = $lang['email_invalid'];
-    }
-    else if (check_field_exists($_POST['user_email'], "user_email", "users") == 1)
-    {
+    } else if (check_field_exists($_POST['user_email'], "user_email", "users") == 1) {
         $err = $lang['email_exist'];
-    }
-    else if (strlen($_POST['user_password']) < 4)
-    {
+    } else if (strlen($_POST['user_password']) < 4) {
         $err = $lang['password_short'];
     }
-    
-    if ($err == '')
-    {
+
+    if ($err == '') {
+
         $request_password = $_POST['user_password'];
         $request_password = md5($request_password);
-        
+
         $sql = "INSERT INTO `users` SET
                `user_email`='" . DB::quote($_POST['user_email']) . "',
                `user_name`='" . DB::quote($_POST['user_name']) . "',
@@ -58,55 +46,45 @@ if (isset($_POST['submit']))
                `user_join_time`='" . $_SERVER['REQUEST_TIME'] . "',
                `user_last_login_time`='" . $_SERVER['REQUEST_TIME'] . "',
                `user_email_verified`='yes'";
-        $result = mysql_query($sql) or mysql_die($sql);
-        $user_id = mysql_insert_id();
-        
-        if ($config['enable_package'] == 'yes')
-        {
+        $user_id = DB::insertGetId($sql);
+
+        if ($config['enable_package'] == 'yes') {
+
             $package_duration = (int) $_POST['user_package_duration'];
             $package_duration_type = $_POST['user_package_duration_type'];
-            
+
             $sql = "SELECT * FROM `packages` WHERE
                    `package_id`='" . (int) $_POST['user_package_id'] . "'";
-            $result = mysql_query($sql) or mysql_die($sql);
-            $tmp = mysql_fetch_assoc($result);
-            
+            $tmp = DB::fetch1($sql);
+
             $expired_time = date("Y-m-d H:i:s", strtotime("+ $package_duration $package_duration_type"));
-            
+
             $sql = "INSERT INTO `subscriber` SET
                    `pack_id`='" . (int) $_POST['user_package_id'] . "',
                    `subscribe_time`='" . date("Y-m-d H:i:s") . "',
                    `expired_time`='$expired_time',
                    `UID`='" . (int) $user_id . "'";
-            mysql_query($sql) or mysql_die($sql);
+            DB::query($sql);
         }
-        
+
         $auto_friend = get_config('signup_auto_friend');
-        
-        if ((strlen($auto_friend) > 1) && (check_field_exists($auto_friend, 'user_name', 'users')))
-        {
+
+        if ((strlen($auto_friend) > 1) && (check_field_exists($auto_friend, 'user_name', 'users'))) {
             $friend = new Friends();
             $friend->make_friends($auto_friend, $_POST['user_name']);
         }
-        
+
         set_message($lang['user_add_success'], 'success');
         $redirect_url = VSHARE_URL . '/admin/user_view.php?user_id=' . $user_id;
         Http::redirect($redirect_url);
     }
 }
 
-if ($config['enable_package'] == 'yes')
-{
+if ($config['enable_package'] == 'yes') {
     $sql = "SELECT * FROM `packages` WHERE
            `package_status`='Active'
             ORDER BY `package_price` DESC";
-    $result = mysql_query($sql) or mysql_die($sql);
-    
-    while ($package = mysql_fetch_assoc($result))
-    {
-        $packages[] = $package;
-    }
-    
+    $packages = DB::fetch($sql);
     $smarty->assign('package', $packages);
 }
 
