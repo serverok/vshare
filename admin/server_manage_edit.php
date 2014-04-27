b637cbf1505ab75e345fde377a0d685c83bd47c8
BuyScripts.in <info@buyscripts.in>
2014-04-27 12:29:52 +0530

    fix

--- admin/server_manage_edit.php
+++ admin/server_manage_edit.php
@@ -17,39 +17,24 @@ require '../include/language/' . LANG . '/lang_admin_server_manage_edit.php';
 
 check_admin_login();
 
-if (isset($_POST['submit']))
-{
-
+if (isset($_POST['submit'])) {
     $server_id = (int) $_POST['server_id'];
-
-    if ($_POST['server_ip'] == '')
-    {
+    if ($_POST['server_ip'] == '') {
         $err = $lang['ip_numeric'];
-    }
-    else if ($_POST['server_url'] == '')
-    {
+    } else if ($_POST['server_url'] == '') {
         $err = $lang['server_url_empty'];
-    }
-    else if ($_POST['user_name'] == '')
-    {
+    } else if ($_POST['user_name'] == '') {
         $err = $lang['user_name_empty'];
-    }
-    else if ($_POST['password'] == '')
-    {
+    } else if ($_POST['password'] == '') {
         $err = $lang['password_empty'];
-    }
-    else if ($_POST['server_type'] == 2 || $_POST['server_type'] == 3)
-    {
+    } else if ($_POST['server_type'] == 2 || $_POST['server_type'] == 3) {
         $server_secdownload_secret = isset($_POST['server_secdownload_secret']) ? $_POST['server_secdownload_secret'] : '';
-
-        if (strlen($server_secdownload_secret) < 10)
-        {
+        if (strlen($server_secdownload_secret) < 10) {
             $err = 'You must enter secdownload.secret';
         }
     }
 
-    if ($err == '')
-    {
+    if ($err == '') {
         $sql = "UPDATE `servers` SET
                `ip`='" . DB::quote($_POST['server_ip']) . "',
                `url`='" . DB::quote($_POST['server_url']) . "',
@@ -59,33 +44,30 @@ if (isset($_POST['submit']))
                `server_type`='" . (int) $_POST['server_type'] . "',
                `server_secdownload_secret`='" . DB::quote($server_secdownload_secret) . "' WHERE
                `id`='" . (int) $server_id . "'";
-        mysql_query($sql) or mysql_die($sql);
-        DB::close();
+        DB::query($sql);
         set_message($lang['server_info_updated'], 'success');
         $redirect_url = VSHARE_URL . '/admin/server_manage.php';
-        Http::redirect($redirect_url);
+
+    } else {
+        set_message($err, 'error');
+        $redirect_url = VSHARE_URL . '/admin/server_manage_edit.php?id=' . $server_id;
     }
 
     DB::close();
-    set_message($err, 'error');
-    $redirect_url = VSHARE_URL . '/admin/server_manage_edit.php?id=' . $server_id;
     Http::redirect($redirect_url);
 }
 
 $server_id = isset($_GET['id']) ? $_GET['id'] : 0;
 
-if (! is_numeric($server_id))
-{
+if (! is_numeric($server_id)) {
     $err = $lang['id_numeric'];
 }
 
 $sql = "SELECT * FROM `servers` WHERE
        `id`='" . (int) $server_id . "'";
+$server_info = DB::fetch1($sql);
 
-$result = mysql_query($sql) or mysql_die($sql);
-$server_info = mysql_fetch_assoc($result);
 $smarty->assign('server_info', $server_info);
-
 $smarty->assign('err', $err);
 $smarty->assign('msg', $msg);
 $smarty->display('admin/header.tpl');
