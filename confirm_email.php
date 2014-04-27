54a444b0118a3649113eed09294143ed612c3455
BuyScripts.in <info@buyscripts.in>
2014-04-28 01:36:33 +0530

    fix

--- confirm_email.php
+++ confirm_email.php
@@ -15,58 +15,46 @@
 require 'include/config.php';
 require 'include/language/' . LANG . '/lang_confirm_email.php';
 
-if (isset($_GET['k']) && isset($_GET['u']) && isset($_GET['i']))
-{
-    if (! is_numeric($_GET['u']))
-    {
+if (isset($_GET['k']) && isset($_GET['u']) && isset($_GET['i'])) {
+    if (! is_numeric($_GET['u'])) {
         $err = $lang['invalid_vcode'];
-    }
-    else
-    {
-        if (! is_numeric($_GET['i']))
-        {
+    } else {
+        if (! is_numeric($_GET['i'])) {
             $err = $lang['invalid_vcode'];
-        }
-        else
-        {
-            if (strlen($_GET['k']) > 40)
-            {
+        } else {
+            if (strlen($_GET['k']) > 40) {
                 $err = $lang['invalid_vcode'];
-            }
-            else
-            {
+            } else {
                 $data1 = 'EMAIL_CHANGE' . $_GET['u'];
                 $sql = "SELECT * FROM `verify_code` WHERE
                        `id`='" . (int) $_GET['i'] . "' AND
                        `vkey`='" . DB::quote($_GET['k']) . "' AND
                        `data1`='" . DB::quote($data1) . "'";
-                $result = mysql_query($sql) or mysql_die($sql);
-                
-                if (mysql_num_rows($result) > 0)
-                {
-                    $tmp = mysql_fetch_assoc($result);
+                $verify_code = DB::fetch1($sql);
+
+                if ($verify_code) {
+
                     $sql = "UPDATE `users` SET
-                           `user_email`='" . DB::quote($tmp['data2']) . "' WHERE
+                           `user_email`='" . DB::quote($verify_code['data2']) . "' WHERE
                            `user_id`='" . (int) $_GET['u'] . "'";
-                    mysql_query($sql) or mysql_die($sql);
-                    $msg = str_replace('[NEW_EMAIL]', $tmp['data2'], $lang['email_changed']);
+                    DB::query($sql);
+
+                    $msg = str_replace('[NEW_EMAIL]', $verify_code['data2'], $lang['email_changed']);
+
                     $sql = "DELETE FROM `verify_code` WHERE
                            `id`='" . (int) $_GET['i'] . "'";
-                    mysql_query($sql) or mysql_die($sql);
+                    DB::quote($sql);
+
                     set_message($msg, 'success');
                     $redirect_url = VSHARE_URL . '/index.php';
                     Http::redirect($redirect_url);
-                }
-                else
-                {
+                } else {
                     $err = $lang['invalid_vcode'];
                 }
             }
         }
     }
-}
-else
-{
+} else {
     $err = $lang['invalid_vcode'];
 }
 
