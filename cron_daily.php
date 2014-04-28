f1685d116f7f00ab7167de16644a69eaa4156b3e
BuyScripts.in <info@buyscripts.in>
2014-04-28 17:10:05 +0530

    fix

--- cron_daily.php
+++ cron_daily.php
@@ -19,24 +19,19 @@ chdir("$current_folder");
 require $current_folder . '/include/config.php';
 require $current_folder . '/include/class.upload_remote.php';
 require $current_folder . '/include/functions_seo_name.php';
-require $current_folder . '/include/class.tags.php';
 require $current_folder . '/include/youtube.php';
-require $current_folder . '/include/class.bulk_import.php';
 require 'Zend/Loader.php';
 Zend_Loader::loadClass('Zend_Gdata_YouTube');
 
 $sql = "SELECT * FROM `import_auto`";
-$result = mysql_query($sql) or mysql_die();
+$auto_import_all = DB::fetch($sql);
 
-if (mysql_num_rows($result) < 1)
-{
+if (! $auto_import_all) {
     exit();
 }
 
-while ($import_auto_info = mysql_fetch_assoc($result))
-{
-    if ($import_auto_info['import_auto_keywords'] != '')
-    {
+foreach ($auto_import_all as $import_auto_info) {
+    if ($import_auto_info['import_auto_keywords'] != '') {
         $import_auto_info['import_auto_keywords'];
         $search_string = $import_auto_info['import_auto_keywords'];
         $yt = new Zend_Gdata_YouTube();
@@ -44,40 +39,32 @@ while ($import_auto_info = mysql_fetch_assoc($result))
         $query->setQuery($search_string);
         $query->setStartIndex(1);
         $query->setMaxResults(10);
-        
+
         $feed = $yt->getVideoFeed($query);
-        
-        foreach ($feed as $entry)
-        {
+
+        foreach ($feed as $entry) {
             $videos[] = $entry->getVideoId();
         }
-        
-        for ($i = 0; $i < count($videos); $i ++)
-        {
-            if (! BulkImport::checkImported($videos[$i], 'youtube'))
-            {
+
+        for ($i = 0; $i < count($videos); $i ++) {
+            if (! BulkImport::checkImported($videos[$i], 'youtube')) {
                 $sql = "INSERT INTO `import_track` SET
 					   `import_track_unique_id`='" . DB::quote($videos[$i]) . "' ,
 					   `import_track_site`='" . DB::quote('youtube') . "'";
-                mysql_query($sql) or mysql_die();
-                
+                DB::query($sql);
+
                 $video_url = 'http://www.youtube.com/watch?v=' . $videos[$i];
                 $video_info = BulkImport::getYoutubeVideoInfo($videos[$i]);
                 $user_name = $import_auto_info['import_auto_user'];
-                
-                $sql = "SELECT * FROM `users` WHERE
-						`user_name`='" . $user_name . "'";
-                
-                $result = mysql_query($sql);
-                $user_info = mysql_fetch_assoc($result);
-                
+
+                $user_info = User::getByName($user_name);
+
                 $channel_id = $import_auto_info['import_auto_channel'];
-                
-                if ($import_auto_info['import_auto_download'] == 0)
-                {
-                    
+
+                if ($import_auto_info['import_auto_download'] == 0) {
+
                     $seo_name = seo_name($video_info['video_title']);
-                    
+
                     $sql = "INSERT INTO `videos` SET
 		                   `video_user_id`='" . (int) $user_info['user_id'] . "',
 		                   `video_title`='" . DB::quote($video_info['video_title']) . "',
@@ -92,27 +79,24 @@ while ($import_auto_info = mysql_fetch_assoc($result))
 		                   `video_add_date`='" . date('Y-m-d') . "',
 		                   `video_active`='1',
 		                   `video_approve`='$config[approve]'";
-                    
-                    mysql_query($sql) or mysql_die();
-                    $vid = mysql_insert_id();
-                    
+
+                    $vid = DB::insertGetId($sql);
+
                     $upload = new upload_remote();
                     $upload->vid = $vid;
                     $upload->url = $video_url;
                     $upload->debug = 1;
-                    
-                    if ($type == 'public' && $config['approve'] == 1)
-                    {
+
+                    if ($type == 'public' && $config['approve'] == 1) {
                         $current_keyword = DB::quote($video_info['video_keywords']);
-                        $tags = new Tags($video_info['video_keywords'], $vid, $user_info['user_id'], "0|$channel_id|0");
+                        $tags = new Tag($video_info['video_keywords'], $vid, $user_info['user_id'], "0|$channel_id|0");
                         $tags->add();
                     }
-                    
+
                     $upload->youtube();
-                
-                }
-                else if ($import_auto_info['import_auto_download'] == 1)
-                {
+
+                } else if ($import_auto_info['import_auto_download'] == 1) {
+
                     $sql = "INSERT INTO `process_queue`SET
 			               `user`='" . DB::quote($user_info['user_name']) . "',
 			               `title`='" . DB::quote($video_info['video_title']) . "',
@@ -122,7 +106,7 @@ while ($import_auto_info = mysql_fetch_assoc($result))
 			               `channels`='0|" . DB::quote($channel_id) . "|0',
 			               `status`='0',
 			               `url`='" . DB::quote($video_url) . "'";
-                    mysql_query($sql) or mysql_die();
+                    DB::query($sql);
                 }
             }
         }
