b2ee160ac653e0a8a3c3dc32984e24c24736ba48
BuyScripts.in <info@buyscripts.in>
2014-04-28 10:51:47 +0530

    fix

--- install/fix_photo_2.7.php
+++ install/fix_photo_2.7.php
@@ -4,24 +4,18 @@ require '../include/config.php';
 
 $dir = VSHARE_DIR . '/photo/';
 
-if (is_dir($dir))
-{
-    if ($dh = opendir($dir))
-    {
-        while (($file = readdir($dh)) !== false)
-        {
-            if (preg_match("/.jpg/i", $file))
-            {
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            if (preg_match("/.jpg/i", $file)) {
                 $user_id = str_replace('.jpg', '',$file);
-                
-                if (preg_match('/^[0123456789]+$/', $user_id))
-                {
+                if (preg_match('/^[0123456789]+$/', $user_id)) {
                     $sql = "UPDATE `users` SET `user_photo`='1' WHERE `user_id`='" . (int) $user_id . "'";
-                    mysql_query($sql) or mysql_die($sql);
+                    DB::query($sql);
                     echo '<p>' . $sql . '</p>';
                 }
             }
-        
+
         }
         closedir($dh);
     }
