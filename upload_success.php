c3f849f0c19915cb1cd031522564eedd4daf7202
BuyScripts.in <info@buyscripts.in>
2014-04-28 11:09:57 +0530

    fix

--- upload_success.php
+++ upload_success.php
@@ -17,125 +17,90 @@ require 'include/language/' . LANG . '/lang_upload_success.php';
 
 $guest_upload = get_config('guest_upload');
 
-if ($guest_upload == 0)
-{
+if ($guest_upload == 0) {
     User::is_logged_in();
 }
 
 $upload_id = $_GET['upload_id'];
 
-if ($upload_id != 'remote')
-{
+if ($upload_id != 'remote') {
+
     $id = $_GET['id'];
-    if (! is_numeric($id))
-    {
+
+    if (! is_numeric($id)) {
         echo 'Invalid id';
         exit(0);
     }
-    
+
     $sql = "SELECT * FROM `process_queue` WHERE
            `id`='" . (int) $id . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $queue_info = mysql_fetch_assoc($result);
+    $queue_info = DB::fetch1($sql);
     $status = $queue_info['status'];
     $user = $queue_info['user'];
-    
-    if ($guest_upload == 0)
-    {
-        if ($user != $_SESSION['USERNAME'])
-        {
+
+    if ($guest_upload == 0) {
+        if ($user != $_SESSION['USERNAME']) {
             echo "Invalid user";
             exit(0);
         }
     }
-    
-    if ($status == 5)
-    {
+
+    if ($status == 5) {
         $video_processed = 1;
         $vid = $queue_info['vid'];
-        
-        $sql = "SELECT * FROM `videos` WHERE
-               `video_id`='" . (int) $vid . "'";
-        $result = mysql_query($sql) or mysql_die();
-        $video_info = mysql_fetch_assoc($result);
+        $video_info = Video::getById($vid);
         $video_flv_name = $video_info['video_flv_name'];
         $video_info['video_thumb_url'] = $servers[$video_info['video_thumb_server_id']];
-        
-        if (mysql_num_rows($result) <= 0)
-        {
+
+        if (! $video_info) {
             $err = $lang['video_not_found'];
         }
-        
+
         $smarty->assign('video_info', $video_info);
-    }
-    else
-    {
+    } else {
         $video_processed = 0;
     }
 
-}
-else
-{
+} else {
     $vid = $_GET['id'];
-    
-    $sql = "SELECT * FROM `videos` WHERE
-           `video_id`='" . (int) $vid . "'";
-    $result = mysql_query($sql) or mysql_die($sql);
-    $video_info = mysql_fetch_assoc($result);
-    
+    $video_info = Video::getById($vid);
+
     $video_flv_name = $video_info['video_flv_name'];
-    
-    if (mysql_num_rows($result) <= 0)
-    {
+
+    if (! $video_info) {
         $err = $lang['video_not_found'];
     }
-    
+
     $smarty->assign('video_info', $video_info);
     $video_processed = 1;
 }
 
-if ($video_processed == 1)
-{
-    if ($video_info['video_vtype'] == 0)
-    {
-        if ($video_info['video_server_id'] == 0)
-        {
+if ($video_processed == 1) {
+    if ($video_info['video_vtype'] == 0) {
+        if ($video_info['video_server_id'] == 0) {
             $flv_url = VSHARE_URL . '/flvideo/' . $video_info['video_folder'] . $video_info['video_flv_name'];
-        }
-        else
-        {
+        } else {
             $sql = "SELECT * FROM `servers` WHERE
                    `id`='" . (int) $video_info['video_server_id'] . "'";
-            $result = mysql_query($sql) or mysql_die($sql);
-            $server_info = mysql_fetch_assoc($result);
+            $server_info = DB::fetch1($sql);
             $flv_url = $server_info['url'] . '/' . $video_info['video_folder'] . $video_info['video_flv_name'];
         }
-        
+
         $smarty->assign('flv_url', $flv_url);
     }
-    
-    if ($video_info['video_active'] == 1 && $video_info['video_approve'] == 1)
-    {
-        if (isset($_SESSION['UID']))
-        {
+
+    if ($video_info['video_active'] == 1 && $video_info['video_approve'] == 1) {
+        if (isset($_SESSION['UID'])) {
 	        update_user_video_count($_SESSION['UID'], 1);
-        }
-        else if ($guest_upload == 1)
-        {
+        } else if ($guest_upload == 1) {
             $guest_upload_user = get_config('guest_upload_user');
-            
-            $sql = "SELECT `user_id` FROM `users` WHERE
-	               `user_name`='" . DB::quote($guest_upload_user) . "'";
-	        $result = mysql_query($sql) or mysql_die($sql);
-	        $user_info = mysql_fetch_assoc($result);
-	        
+	        $user_info = User::getByName($guest_upload_user);
 	        update_user_video_count($user_info['user_id'], 1);
         }
     }
 }
 
-if ($upload_id != 'remote')
-{
+if ($upload_id != 'remote') {
     unset($_SESSION["$upload_id"]['field_privacy']);
     unset($_SESSION["$upload_id"]['description']);
     unset($_SESSION["$upload_id"]['title']);
