ba711a266bb505cf0a5a0161391645529c07f51f
BuyScripts.in <info@buyscripts.in>
2014-04-29 15:55:10 +0530

    class Xss updated to use HTML Purifier 4.6.0

--- include/classes/Xss.php
+++ include/classes/Xss.php
@@ -10,12 +10,12 @@ class Xss
         if (get_config('allow_html')) {
             require VSHARE_DIR . '/include/htmlpurifier/HTMLPurifier.auto.php';
             $config = HTMLPurifier_Config::createDefault();
-            $config->set('HTML', 'DefinitionID', 'User Comment Filter');
-            $config->set('HTML', 'DefinitionRev', 1);
-            $config->set('HTML', 'Doctype', 'XHTML 1.0 Transitional');
-            $config->set('HTML', 'Allowed', 'a[href|title],img[src|alt|width|height],em,strong,p,pre,b');
-            $config->set('AutoFormat', 'Linkify', true);
-            $config->set('AutoFormat', 'AutoParagraph', true);
+            $config->set('HTML.DefinitionID', 'User Comment Filter');
+            $config->set('HTML.DefinitionRev', 1);
+            $config->set('HTML.Doctype', 'XHTML 1.0 Transitional');
+            $config->set('HTML.Allowed', 'a[href|title],img[src|alt|width|height],em,strong,p,pre,b');
+            $config->set('AutoFormat.Linkify', true);
+            $config->set('AutoFormat.AutoParagraph', true);
             $purifier = new HTMLPurifier($config);
             return $purifier->purify($string);
         } else {
