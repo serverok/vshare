86a23c0fc998e4f9c9ff498139ef994f40883a0b
BuyScripts.in <info@buyscripts.in>
2014-04-30 14:32:26 +0530

    format insert functions

--- include/functions_insert.php
+++ include/functions_insert.php
@@ -27,20 +27,13 @@ function insert_tags()
 
 function insert_show_videos($a)
 {
-    if ($a['type'] == "top")
-    {
+    if ($a['type'] == "top") {
         $query = "ORDER BY `video_view_number` DESC";
-    }
-    else if ($a['type'] == 'new')
-    {
+    } else if ($a['type'] == 'new') {
         $query = "ORDER BY `video_add_time` DESC";
-    }
-    else if ($a['type'] == 'recently_viewed')
-    {
+    } else if ($a['type'] == 'recently_viewed') {
         $query = "ORDER BY `video_view_time` DESC";
-    }
-    else if ($a['type'] == 'featured')
-    {
+    } else if ($a['type'] == 'featured') {
         $query = "AND `video_featured`='yes' ORDER BY `video_add_time` DESC";
     }
 
@@ -51,8 +44,7 @@ function insert_show_videos($a)
             LIMIT " . (int) $a['num_videos'];
     $result = mysql_query($sql) or mysql_die($sql);
 
-    if ($a['type'] == 'featured' && mysql_num_rows($result) < 1)
-    {
+    if ($a['type'] == 'featured' && mysql_num_rows($result) < 1) {
         $sql = "SELECT * FROM videos WHERE
                `video_type`='public' AND
                `video_active`='1' AND
@@ -61,8 +53,7 @@ function insert_show_videos($a)
         $result = mysql_query($sql) or mysql_die($sql);
     }
 
-    while ($video_info = mysql_fetch_assoc($result))
-    {
+    while ($video_info = mysql_fetch_assoc($result)) {
         $videos[] = $video_info;
     }
 
@@ -85,52 +76,45 @@ function insert_time_range($info)
     $addtime = $info['time'];
     $interval = $present - $addtime;
 
-    if ($interval > 0)
-    {
+    if ($interval > 0) {
         $day = $interval / (60 * 60 * 24);
-        if ($day >= 1)
-        {
+
+        if ($day >= 1) {
             $range = floor($day) . ' days ';
             $interval = $interval - (60 * 60 * 24 * floor($day));
         }
-        if ($interval > 0 && $range == '')
-        {
+
+        if ($interval > 0 && $range == '') {
             $hour = $interval / (60 * 60);
-            if ($hour >= 1)
-            {
+            if ($hour >= 1) {
                 $range = floor($hour) . ' hours ';
                 $interval = $interval - (60 * 60 * floor($hour));
             }
         }
-        if ($interval > 0 && $range == '')
-        {
+
+        if ($interval > 0 && $range == '') {
             $min = $interval / (60);
-            if ($min >= 1)
-            {
+            if ($min >= 1) {
                 $range = floor($min) . ' minutes ';
                 $interval = $interval - (60 * floor($min));
             }
         }
-        if ($interval > 0 && $range == '')
-        {
+
+        if ($interval > 0 && $range == '') {
             $scn = $interval;
-            if ($scn >= 1)
-            {
+            if ($scn >= 1) {
                 $range = $scn . ' seconds ';
             }
         }
-        if ($range != '')
-        {
+
+        if ($range != '') {
             $range = $range . ' ago';
-        }
-        else
-        {
+        } else {
             $range = 'just now';
         }
+
         return $range;
-    }
-    else
-    {
+    } else {
         return '1 seconds ago';
     }
 }
@@ -146,12 +130,9 @@ function insert_video_channel($a)
 {
     $a['tbl'] = isset($a['tbl']) ? $a['tbl'] : '';
 
-    if ($a['tbl'] == '')
-    {
+    if ($a['tbl'] == '') {
         $sqlx = "`video_channels` AS `channel` FROM `videos` WHERE `video_id`='" . (int) $a['vid'] . "'";
-    }
-    else
-    {
+    } else {
         $sqlx = "`group_channels` FROM `groups` WHERE `group_id`='" . (int) $a['gid'] . "'";
     }
 
@@ -181,44 +162,34 @@ function insert_show_rate($a)
     $list = '';
     $rate = $a['rte'];
     $rating = $a['rated'];
-    if ($rate != 0)
-    {
+
+    if ($rate != 0) {
         $rate = $rate / $rating;
         $num_full_star = floor($rate);
 
-        for ($i = 0; $i < $num_full_star; $i ++)
-        {
+        for ($i = 0; $i < $num_full_star; $i ++) {
             $list .= '<img src="' . VSHARE_URL . '/templates/images/star.gif" alt="star" />&nbsp;';
         }
 
-        if ($rate == $num_full_star)
-        {
+        if ($rate == $num_full_star) {
             $num_falf_star = 0;
-        }
-        else
-        {
+        } else {
             $num_falf_star = 1;
             $list .= '<img src="' . VSHARE_URL . '/templates/images/half_star.gif" alt="half star" />';
         }
 
         $num_blank_star = 5 - $num_full_star - $num_falf_star;
 
-        for ($i = 0; $i < $num_blank_star; $i ++)
-        {
+        for ($i = 0; $i < $num_blank_star; $i ++) {
             $list .= '<img src="' . VSHARE_URL . '/templates/images/blank_star.gif" alt="blank star" />';
         }
-    }
-    else
-    {
+    } else {
         $rate = 0;
     }
 
-    if ($rate > 0)
-    {
+    if ($rate > 0) {
         return $list;
-    }
-    else
-    {
+    } else {
         return 'Not yet rated';
     }
 }
@@ -233,8 +204,8 @@ function insert_row_count($a)
         'group_videos',
         'group_topics'
     );
-    if (! in_array($a['table'], $table_arr))
-    {
+
+    if (! in_array($a['table'], $table_arr)) {
         $execute_query = 0;
     }
 
@@ -243,8 +214,8 @@ function insert_row_count($a)
         'group_video_group_id',
         'group_topic_group_id'
     );
-    if (! in_array($a['field1'], $field_arr1))
-    {
+
+    if (! in_array($a['field1'], $field_arr1)) {
         $execute_query = 0;
     }
 
@@ -253,20 +224,17 @@ function insert_row_count($a)
         'group_video_approved',
         'group_topic_approved'
     );
-    if (! in_array($a['field2'], $field_arr2))
-    {
+
+    if (! in_array($a['field2'], $field_arr2)) {
         $execute_query = 0;
     }
 
-    if ($execute_query == 1)
-    {
+    if ($execute_query == 1) {
         $sql = "SELECT count(*) AS `total` FROM `$a[table]` WHERE
 	           `$a[field1]`='" . (int) $a['group_id'] . "' AND
                `$a[field2]`='yes'";
         return DB::getTotal($sql);
-    }
-    else
-    {
+    } else {
         return 'error';
     }
 }
@@ -325,8 +293,7 @@ function insert_comment_info($a)
            `comment_video_id`='" . (int) $a['vid'] . "'
             ORDER BY `comment_id` ASC";
     $result = mysql_query($sql) or mysql_die($sql);
-    while ($comment_info = mysql_fetch_assoc($result))
-    {
+    while ($comment_info = mysql_fetch_assoc($result)) {
         $comment_info['comment_text'] = nl2br($comment_info['comment_text']);
         $comments[] = $comment_info;
     }
@@ -345,12 +312,9 @@ function insert_video_count($a)
     $add = '';
     $a['type'] = isset($a['type']) ? $a['type'] : 'public';
 
-    if ($a['type'] == 'public')
-    {
+    if ($a['type'] == 'public') {
         $add = " AND `video_type`='public'";
-    }
-    else if ($a['type'] == 'private')
-    {
+    } else if ($a['type'] == 'private') {
         $add = " AND `video_type`='private'";
     }
 
@@ -410,25 +374,21 @@ function insert_group_count($a)
     $a['uid'] = isset($a['uid']) ? $a['uid'] : '';
     $a['type'] = isset($a['type']) ? $a['type'] : 'public';
 
-    if ($a['chid'] != '')
-    {
+    if ($a['chid'] != '') {
         $from = 'groups';
         $add1 = "WHERE `group_channels` LIKE '%|$a[chid]|%' ";
     }
 
-    if ($a['uid'] != '')
-    {
+    if ($a['uid'] != '') {
         $add1 = "WHERE m.group_member_group_id=o.group_id AND m.group_member_user_id='" . (int) $a['uid'] . "'";
         $from = "`groups` AS o,`group_members` AS m";
     }
 
-    if ($a['type'] == 'public')
-    {
+    if ($a['type'] == 'public') {
         $add2 = "AND `group_type`='public'";
     }
 
-    if ($a['type'] == 'private')
-    {
+    if ($a['type'] == 'private') {
         $add2 = "AND `group_type`='private'";
     }
 
@@ -442,29 +402,24 @@ function insert_group_info_count($a)
     $sql_extra = '';
     $execute_query = 1;
 
-    if (isset($a['query']))
-    {
-        if ($a['query'] == 1)
-        {
+    if (isset($a['query'])) {
+        if ($a['query'] == 1) {
             $sql_extra = "AND `$a[field1]`='yes'";
         }
     }
 
-    if ($a['tbl'] == 'groups')
-    {
+    if ($a['tbl'] == 'groups') {
         $sql = "SELECT count(*) AS `total` FROM `groups` WHERE
                `group_id`='" . (int) $a['gid'] . "' " . $sql_extra;
         return DB::getTotal($sql);
-    }
-    else
-    {
+    } else {
         $table_arr = array(
             'group_members',
             'group_videos',
             'group_topics'
         );
-        if (! in_array($a['tbl'], $table_arr))
-        {
+
+        if (! in_array($a['tbl'], $table_arr)) {
             $execute_query = 0;
         }
 
@@ -473,8 +428,8 @@ function insert_group_info_count($a)
             'group_video_approved',
             'group_topic_approved'
         );
-        if (! in_array($a['field1'], $field_arr1))
-        {
+
+        if (! in_array($a['field1'], $field_arr1)) {
             $execute_query = 0;
         }
 
@@ -483,19 +438,16 @@ function insert_group_info_count($a)
             'group_video_group_id',
             'group_topic_group_id'
         );
-        if (! in_array($a['field2'], $field_arr2))
-        {
+
+        if (! in_array($a['field2'], $field_arr2)) {
             $execute_query = 0;
         }
 
-        if ($execute_query == 1)
-        {
+        if ($execute_query == 1) {
             $sql = "SELECT count(*) AS `total` FROM `$a[tbl]` WHERE
     				`$a[field2]`='" . (int) $a['gid'] . "' " . $sql_extra;
             return DB::getTotal($sql);
-        }
-        else
-        {
+        } else {
             return "error";
         }
     }
@@ -533,34 +485,28 @@ function insert_group_image($a)
            `group_id`='" . (int) $a['gid'] . "'";
     $tmp = DB::fetch1($sql);
 
-    if ($tmp['group_image'] == 'owner_only')
-    {
+    if ($tmp['group_image'] == 'owner_only') {
         $group_image_video_id = $tmp['group_image_video'];
-    }
-    else
-    {
+    } else {
         $sql = "SELECT `group_video_video_id` FROM " . DB::quote($a['tbl']) . " WHERE
                `group_video_group_id`='" . (int) $a['gid'] . "'
                 ORDER BY `AID` DESC
                 LIMIT 1";
         $tmp = DB::fetch1($sql);
-        if ($tmp)
-        {
+
+        if ($tmp) {
             $group_image_video_id = $tmp['group_video_video_id'];
         }
     }
 
-    if ($group_image_video_id != 0)
-    {
+    if ($group_image_video_id != 0) {
         $sql = "SELECT video_id,video_folder,video_thumb_server_id FROM `videos` WHERE
     			`video_id`=$group_image_video_id";
         $tmp = DB::fetch1($sql);
 
         $tmp['video_thumb_url'] = $servers[$tmp['video_thumb_server_id']];
         return $tmp;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 
@@ -608,15 +554,13 @@ function insert_member_img($a)
 function insert_check_group_mem($a)
 {
     global $conn;
-    if ($_SESSION['UID'] != '')
-    {
+
+    if ($_SESSION['UID'] != '') {
         $sql = "SELECT count(*) AS `total` FROM `group_members` WHERE
                `group_member_group_id`='" . (int) $a['gid'] . "' AND
                `group_member_user_id`='" . (int) $_SESSION['UID'] . "'";
         return DB::getTotal($sql);
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
@@ -638,52 +582,30 @@ function insert_timediff($var)
     $x['hours'] = $hours;
     $x['minutes'] = $minutes;
     $x['seconds'] = $second;
-    if ($years == 1)
-    {
+
+    if ($years == 1) {
         echo "$years year ago";
-    }
-    else if ($years > 1)
-    {
+    } else if ($years > 1) {
         echo "$years years ago";
-    }
-    else if ($months == 1)
-    {
+    } else if ($months == 1) {
         echo "$months month ago";
-    }
-    else if ($months > 1)
-    {
+    } else if ($months > 1) {
         echo "$months months ago";
-    }
-    else if ($days == 1)
-    {
+    } else if ($days == 1) {
         echo "$days day ago";
-    }
-    else if ($days > 1)
-    {
+    } else if ($days > 1) {
         echo "$days days ago";
-    }
-    else if ($hours == 1)
-    {
+    } else if ($hours == 1) {
         echo "$hours hour ago";
-    }
-    else if ($hours > 1)
-    {
+    } else if ($hours > 1) {
         echo "$hours hours ago";
-    }
-    else if ($minutes == 1)
-    {
+    } else if ($minutes == 1) {
         echo "$minutes minute ago";
-    }
-    else if ($minutes > 1)
-    {
+    } else if ($minutes > 1) {
         echo "$minutes minutes ago";
-    }
-    else if ($second == 1)
-    {
+    } else if ($second == 1) {
         echo "$second second ago";
-    }
-    else if ($second > 1)
-    {
+    } else if ($second > 1) {
         echo "$second seconds ago";
     }
 }
@@ -710,8 +632,8 @@ function insert_getfield($v)
         'videos',
         'users'
     );
-    if (! in_array($v['table'], $table_arr))
-    {
+
+    if (! in_array($v['table'], $table_arr)) {
         $execute_query = 0;
     }
 
@@ -723,8 +645,8 @@ function insert_getfield($v)
         'video_user_id',
         'user_website'
     );
-    if (! in_array($v['field'], $field_arr))
-    {
+
+    if (! in_array($v['field'], $field_arr)) {
         $execute_query = 0;
     }
 
@@ -735,31 +657,28 @@ function insert_getfield($v)
         'group_owner_id',
         'user_id'
     );
-    if (! in_array($v['qfield'], $qfield_arr))
-    {
+
+    if (! in_array($v['qfield'], $qfield_arr)) {
         $execute_query = 0;
     }
 
-    if (isset($v['order']))
-    {
+    if (isset($v['order'])) {
+
         $order_arr = array(
             'order by group_topic_post_id desc',
             'order by video_id desc',
             'order by user_id desc',
             'order by group_id desc'
         );
-        if (! in_array(strtolower($v['order']), $order_arr))
-        {
+
+        if (! in_array(strtolower($v['order']), $order_arr)) {
             $execute_query = 0;
         }
-    }
-    else
-    {
+    } else {
         $v['order'] = '';
     }
 
-    if ($execute_query == 1)
-    {
+    if ($execute_query == 1) {
         $sql = "SELECT `$v[field]` FROM `$v[table]` WHERE
                `$v[qfield]`='" . DB::quote($v['qvalue']) . "' " . DB::quote($v['order']);
         $tmp = DB::fetch1($sql);
@@ -770,18 +689,12 @@ function insert_getfield($v)
 function insert_format_size($v)
 {
     $size = $v['size'];
-    if ($v['type'] == 'byte')
-    {
+    if ($v['type'] == 'byte') {
 
-    }
-    else
-    {
-        if ($size < 1024)
-        {
+    } else {
+        if ($size < 1024) {
             $output = round($size, 2) . ' MB';
-        }
-        else if ($size < 1024 * 1024)
-        {
+        } else if ($size < 1024 * 1024) {
             $output = round($size / 1024, 2) . ' GB';
         }
     }
