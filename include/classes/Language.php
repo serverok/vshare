5c291d9578678568f00ddd8cde0ec98cb0c27744
BuyScripts.in <info@buyscripts.in>
2014-04-30 11:42:54 +0530

    code format class Language. fixes #516

--- include/classes/Language.php
+++ include/classes/Language.php
@@ -6,14 +6,12 @@ class Language
     {
         language::validate($lang);
 
-        if (isset($_SERVER['HTTP_REFERER']))
-        {
+        if (isset($_SERVER['HTTP_REFERER'])) {
             $redirect_url = $_SERVER['HTTP_REFERER'];
         }
 
-        if (stripos($redirect_url, VSHARE_URL) === false)
-        {
-            $redirect_url = VSHARE_URL . '/index.php';
+        if (stripos($redirect_url, VSHARE_URL) === false) {
+            $redirect_url = VSHARE_URL . '/';
         }
 
         Http::redirect($redirect_url);
@@ -27,10 +25,10 @@ class Language
             'en', 'fr'
         );
 
-        if (! in_array($lang, $allowedLaguages))
-        {
+        if (! in_array($lang, $allowedLaguages)) {
             $lang = $language;
         }
+
         $_SESSION['LANG'] = $lang;
         setcookie('LANG', $lang, time() + 2592000); /* expire in 30 days */
     }
