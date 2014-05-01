c64990352f5f9366f19a7aa2057d8a816857e316
BuyScripts.in <info@buyscripts.in>
2014-05-01 11:45:43 +0530

    disable upload progress bar by default.

--- include/settings/upload.php
+++ include/settings/upload.php
@@ -2,6 +2,6 @@
 
 # List of allowed file Types
 
-$file_types = array("flv","3gp","mp4","mov","asf","mpg","avi","mpeg","wmv","rm","dat","m4v");
-$use_upload_progress_bar = 1;
+$file_types = array('flv','3gp','mp4','mov','asf','mpg','avi','mpeg','wmv','rm','dat','m4v');
+$use_upload_progress_bar = 0;
 
