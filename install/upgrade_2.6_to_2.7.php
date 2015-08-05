82be4ff256164cc09eb255caa89399f07e93bf63
BuyScripts.in <info@buyscripts.in>
2015-08-05 22:24:57 +0530

    changed [VSHARE_VERSION] to VSHARE_VERSION_NUMBER_HERE

--- install/upgrade_2.6_to_2.7.php
+++ install/upgrade_2.6_to_2.7.php
@@ -3,7 +3,7 @@
  *
  *   COMPANY: BuyScripts.in
  *   PROJECT: vShare Youtube Clone
- *   VERSION: [VSHARE_VERSION]
+ *   VERSION: VSHARE_VERSION_NUMBER_HERE
  *   LICENSE: http://buyscripts.in/vshare-license
  *   WEBSITE: http://buyscripts.in/vshare-youtube-clone
  *
