0d700a2b353468f06ec31d64a90e1bbbd16ac06d
Yujin Boby <info@bizhat.com>
2015-09-01 13:42:16 +0530

    utf8 and MyISAM for tables.

--- install/sql/upgrade_3.1_to_3.2.sql
+++ install/sql/upgrade_3.1_to_3.2.sql
@@ -1,13 +1,11 @@
 CREATE TABLE IF NOT EXISTS `episodes` (
-  `episode_id` int(11) NOT NULL AUTO_INCREMENT,
-  `episode_name` varchar(255) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`episode_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+  `episode_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `episode_name` varchar(255) NOT NULL
+) ENGINE=MYISAM  DEFAULT CHARSET=utf8 ;
 
 CREATE TABLE IF NOT EXISTS `episode_videos` (
-  `ep_video_id` int(11) NOT NULL AUTO_INCREMENT,
+  `ep_video_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `ep_video_eid` int(11) NOT NULL DEFAULT '0',
   `ep_video_vid` int(11) NOT NULL DEFAULT '0',
-  `ep_video_order` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ep_video_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+  `ep_video_order` int(11) NOT NULL DEFAULT '0'
+) ENGINE=MYISAM DEFAULT CHARSET=utf8 ;
