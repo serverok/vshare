f7e3794dacb1866fca9d44373300c0dedf5d8388
Yujin Boby <info@bizhat.com>
2015-11-01 21:27:19 +0530

    video convert show actual error message

--- tools/video_convert.php
+++ tools/video_convert.php
@@ -1,15 +1,11 @@
-<html>
-<body>
 <?php
 
 error_reporting(E_ALL);
+
 $convert_command = "REPLACE WITH YOUR CONVERT COMMAND FROM templates_c/debug.txt";
-$var = exec($convert_command,$exec_result);
-for($i=0;$i<count($exec_result);$i++)
-{
+
+$var = exec($convert_command . ' 2>&1', $exec_result);
+
+for ($i=0;$i<count($exec_result);$i++) {
   echo $exec_result[$i] . "<br>";
 }
-
-?>
-</body>
-</html>
